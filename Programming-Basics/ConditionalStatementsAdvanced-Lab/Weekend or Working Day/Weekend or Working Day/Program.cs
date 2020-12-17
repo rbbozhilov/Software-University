957e4622285b8c089ba32d010c9d7171a89d514f
Rumen <rbbojilov@gmail.com>
2020-12-17 12:32:01 +0200

    Add

---
+++ Programming-Basics/ConditionalStatementsAdvanced-Lab/Weekend or Working Day/Weekend or Working Day/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Weekend_or_Working_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            switch (number)
+            {
+
+                case "Monday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+                    }
+
+                case "Tuesday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+                    }
+                case "Wednesday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+                    }
+                case "Thursday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+                    }
+                case "Friday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+                    }
+                case "Saturday":
+                    {
+                        Console.WriteLine("Weekend");
+                        break;
+                    }
+                case "Sunday":
+                    {
+                        Console.WriteLine("Weekend");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Error");
+                        break;
+                    }
+            }
+
+        }
+    }
+}
---
+++ Programming-Basics/ConditionalStatementsAdvanced-Lab/Working Hours/Working Hours/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if ((day == "Monday" || day == "Tuesday" || day == "Wendsday" || day == "Thursday" || day == "Friday" || day == "Saturday") && (hour >= 10 && hour <= 18))
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
