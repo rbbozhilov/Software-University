86915d0392f84c5a35a6263e22a791ad17f0d54a
Rumen <rbbojilov@gmail.com>
2020-12-19 11:03:39 +0200

    Add more exercise from conditional statements advanced

---
+++ Programming-Basics/ConditionalStatementsAdvanced-Exercise/Hotel Room/Hotel Room/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Hotel_Room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int noshtuvki = int.Parse(Console.ReadLine());
+            double sumstudio = 0;
+            double sumapartment = 0;
+
+            switch (month)
+            {
+
+
+                case "May":
+                    {
+                        sumstudio = 50 * noshtuvki;
+                        sumapartment = 65 * noshtuvki;
+
+
+                        if (noshtuvki > 14)
+                        {
+                            sumapartment -= sumapartment * 0.1;
+                            sumstudio -= sumstudio * 0.3;
+                        }
+                        else if (noshtuvki > 7 && noshtuvki <= 14)
+                        {
+                            sumstudio -= sumstudio * 0.05;
+                        }
+
+                        break;
+                    }
+                case "June":
+                    {
+
+                        sumstudio = 75.20 * noshtuvki;
+                        sumapartment = 68.70 * noshtuvki;
+
+
+                        if (noshtuvki > 14)
+                        {
+                            sumapartment -= sumapartment * 0.1;
+                            sumstudio -= sumstudio * 0.2;
+                        }
+
+
+
+                        break;
+                    }
+                case "July":
+                    {
+
+                        sumstudio = 76 * noshtuvki;
+                        sumapartment = 77 * noshtuvki;
+
+                        if (noshtuvki > 14)
+                        {
+                            sumapartment -= sumapartment * 0.1;
+
+                        }
+
+
+
+                        break;
+                    }
+                case "August":
+                    {
+
+                        sumstudio = 76 * noshtuvki;
+                        sumapartment = 77 * noshtuvki;
+
+                        if (noshtuvki > 14)
+                        {
+                            sumapartment -= sumapartment * 0.1;
+
+                        }
+
+
+
+                        break;
+                    }
+                case "September":
+                    {
+
+
+                        sumstudio = 75.20 * noshtuvki;
+                        sumapartment = 68.70 * noshtuvki;
+
+
+                        if (noshtuvki > 14)
+                        {
+                            sumapartment -= sumapartment * 0.1;
+                            sumstudio -= sumstudio * 0.2;
+                        }
+
+
+
+                        break;
+                    }
+                case "October":
+                    {
+
+                        sumstudio = 50 * noshtuvki;
+                        sumapartment = 65 * noshtuvki;
+
+
+                        if (noshtuvki > 14)
+                        {
+                            sumapartment -= sumapartment * 0.1;
+                            sumstudio -= sumstudio * 0.3;
+                        }
+                        else if (noshtuvki > 7 && noshtuvki <= 14)
+                        {
+                            sumstudio -= sumstudio * 0.05;
+                        }
+
+
+                        break;
+                    }
+            }
+
+            Console.WriteLine("Apartment: {0:F2} lv.\nStudio: {1:F2} lv.", sumapartment, sumstudio);
+        }
+    }
+}
---
+++ Programming-Basics/ConditionalStatementsAdvanced-Exercise/Journey/Journey/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            string sezon = Console.ReadLine();
+
+            double sum = 0;
+
+            switch (sezon)
+            {
+
+                case "summer":
+                    {
+                        if (budjet <= 100)
+                        {
+
+                            sum = budjet * 0.3;
+
+                            Console.WriteLine("Somewhere in Bulgaria\nCamp - {0:F2}", sum);
+
+                        }
+                        else if (budjet > 100 && budjet <= 1000)
+                        {
+                            sum = budjet * 0.4;
+                            Console.WriteLine("Somewhere in Balkans\nCamp - {0:F2}", sum);
+                        }
+                        else if (budjet > 1000)
+                        {
+                            sum = (budjet * 0.9);
+                            Console.WriteLine("Somewhere in Europe\nHotel - {0:F2}", sum);
+                        }
+
+
+                        break;
+                    }
+
+
+                case "winter":
+                    {
+                        if (budjet <= 100)
+                        {
+
+                            sum = (budjet * 0.7);
+
+                            Console.WriteLine("Somewhere in Bulgaria\nHotel - {0:F2}", sum);
+
+                        }
+                        else if (budjet > 100 && budjet <= 1000)
+                        {
+                            sum = (budjet * 0.8);
+                            Console.WriteLine("Somewhere in Balkans\nHotel - {0:F2}", sum);
+                        }
+                        else if (budjet > 1000)
+                        {
+                            sum = (budjet * 0.9);
+                            Console.WriteLine("Somewhere in Europe\nHotel - {0:F2}", sum);
+                        }
+
+
+                        break;
+                    }
+
+            }
+        }
+    }
+}
