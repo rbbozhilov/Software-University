dcf54cb2b6450f9365fad8fe9dcbbfe16c8c97ec
Rumen <rbbojilov@gmail.com>
2020-12-21 13:32:56 +0200

    Add

---
+++ Programming-Basics/ConditionalStatements -Exercise/Scholarship/Scholarship/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dohod = double.Parse(Console.ReadLine());
+            double uspeh = double.Parse(Console.ReadLine());
+            double minzaplata = double.Parse(Console.ReadLine());
+            double stipendiaotlichen = Math.Floor(uspeh * 25);
+            double socstipendia = Math.Floor(minzaplata * 0.35);
+
+
+            if (dohod >= minzaplata && uspeh >= 5.5)
+            {
+                Console.WriteLine("You get a scholarship for excellent results {0} BGN", stipendiaotlichen);
+            }
+            else if (dohod >= minzaplata && uspeh < 5.5)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+            else if (dohod < minzaplata && uspeh >= 5.5 && socstipendia <= stipendiaotlichen)
+            {
+                Console.WriteLine("You get a scholarship for excellent results {0} BGN", stipendiaotlichen);
+            }
+            else if (dohod < minzaplata && uspeh > 4.5)
+            {
+                Console.WriteLine("You get a Social scholarship {0} BGN", socstipendia);
+            }
+            else if (dohod < minzaplata && uspeh <= 4.5)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+        }
+    }
+}
