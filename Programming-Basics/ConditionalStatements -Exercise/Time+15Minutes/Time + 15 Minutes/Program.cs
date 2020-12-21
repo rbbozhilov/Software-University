826fd0bfad84492f8207e6262c8ddd3d205d3584
Rumen <rbbojilov@gmail.com>
2020-12-21 13:30:32 +0200

    Add some more exercise

---
+++ Programming-Basics/ConditionalStatements -Exercise/Time+15Minutes/Time + 15 Minutes/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Time___15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int hours1;
+            hours1 = hours;
+            int k = minutes + 15;
+            int helper = 0;
+
+            int lastminute = 0;
+            if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59)
+            {
+                if (k >= 60)
+                {
+
+                    for (int i = minutes; i <= k; i++)
+                    {
+
+                        helper = i;
+
+                        if (helper == 60 && i == k)
+                        {
+                            lastminute = 0;
+                            if (hours == 23)
+                            {
+                                hours = 0;
+                            }
+
+                        }
+
+                        else if (helper > 60)
+                        {
+                            lastminute++;
+
+                        }
+
+                    }
+
+                    if ((k >= 60 || k == 60) && hours == 23)
+                    {
+                        hours = 0;
+                    }
+
+
+
+                    if (k >= 60 && hours1 != 23)
+                    {
+                        hours++;
+                    }
+
+
+
+                    if (lastminute >= 0 && lastminute < 10)
+                    {
+                        Console.WriteLine("{0}:0{1}", hours, lastminute);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0}:{1}", hours, lastminute);
+                    }
+                }
+                else
+                {
+                    if (k >= 0 && k < 10)
+                    {
+                        Console.WriteLine("{0}:0{1}", hours, k);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0}:{1}", hours, k);
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Programming-Basics/ConditionalStatements -Exercise/WorldSwimmingRecord/World Swimming Record/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordConcored = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double vreme = double.Parse(Console.ReadLine());
+
+            double timeDistance = vreme * distance;
+            double late = Math.Floor(distance / 15) * 12.5;
+            double fulltime = timeDistance + late;
+
+            if (fulltime < recordConcored)
+            {
+                Console.WriteLine("Yes, he succeeded! The new world record is {0:F2} seconds.", fulltime);
+            }
+            else
+            {
+                Console.WriteLine("No, he failed! He was {0:F2} seconds slower.", fulltime - recordConcored);
+
+            }
+        }
+    }
+}
