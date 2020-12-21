2bfef30ed0e39cf1c454ae6627939aff87c0fa6d
Rumen <rbbojilov@gmail.com>
2020-12-21 13:32:02 +0200

    Add

---
+++ Programming-Basics/ConditionalStatements -Exercise/SumSeconds/SumSeconds/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalTime = firstTime + secondTime + thirdTime;
+
+            int minute = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine("{0}:0{1}", minute, seconds);
+
+            }
+            else
+            {
+                Console.WriteLine("{0}:{1}", minute, seconds);
+
+            }
+        }
+    }
+}
