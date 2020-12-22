cdee50c3f06ffaa34737d833aecfbacd9c79790a
Rumen <rbbojilov@gmail.com>
2020-12-22 11:30:10 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-28and29March2020/Suitcases Load/Suitcases Load/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Suitcases_Load
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capacityBagage = double.Parse(Console.ReadLine());
+
+
+
+            string text = string.Empty;
+            int countSuccess = 0;
+            int counterOfInputs = 0;
+            bool flag = false;
+            while ((text = Console.ReadLine()) != "End")
+            {
+
+                double currentObem = double.Parse(text);
+
+                counterOfInputs++;
+
+
+                if (counterOfInputs % 3 == 0)
+                {
+                    capacityBagage -= currentObem + (currentObem * 0.1);
+                }
+                else
+                {
+                    capacityBagage -= currentObem;
+                }
+
+
+                if (capacityBagage >= 0)
+                {
+                    countSuccess++;
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                    break;
+                }
+
+
+
+            }
+
+
+            if (flag)
+            {
+
+                Console.WriteLine("Congratulations! All suitcases are loaded!\nStatistic: {0} suitcases loaded.", countSuccess);
+
+            }
+            else
+            {
+                Console.WriteLine("No more space!\nStatistic: {0} suitcases loaded.", countSuccess);
+            }
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-28and29March2020/Supplies for School/Supplies for School/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double paketxim = 5.80;
+            double paketmarkeri = 7.20;
+            double preparat = 1.20;
+
+
+            int broihimikai = int.Parse(Console.ReadLine());
+            int broimarkeri = int.Parse(Console.ReadLine());
+            double litarpreparat = double.Parse(Console.ReadLine());
+            int procentnamalenie = int.Parse(Console.ReadLine());
+
+            double cenahimikalki = paketxim * broihimikai;
+            double cenamarkeri = paketmarkeri * broimarkeri;
+            double cenapreparat = preparat * litarpreparat;
+            double obshtasum = cenahimikalki + cenamarkeri + cenapreparat;
+
+            double obshtonamalenie = obshtasum - (obshtasum * procentnamalenie / 100);
+
+
+            Console.WriteLine("{0:F3}", obshtonamalenie);
+        }
+    }
+}
