1a7d33d8ed91feb2ab0215b9dc5dea6cad34a522
Rumen <rbbojilov@gmail.com>
2020-12-22 11:25:41 +0200

    Add

---
+++ Programming-Basics/ConditionalStatements -Exercise/Godzilla vs. Kong/Godzilla vs. Kong/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int countOfStatists = int.Parse(Console.ReadLine());
+            double priceForClotes = double.Parse(Console.ReadLine());
+            double priceForDecor = budget * 0.10;
+            double priceForClothing = countOfStatists * priceForClotes;
+
+
+
+            if (countOfStatists > 150)
+            {
+                priceForClothing = priceForClothing - (priceForClothing * 0.10);
+            }
+            double allThogether = priceForDecor + priceForClothing;
+            double leftMoney = budget - allThogether;
+            double neededMoney = allThogether - budget;
+            if (budget >= allThogether)
+            {
+                if (budget >= allThogether)
+                    Console.WriteLine("Action!");
+                Console.WriteLine("Wingard starts filming with {0:F2} leva left.", leftMoney);
+            }
+            else if (budget < allThogether)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine("Wingard needs {0:F2} leva more.", neededMoney);
+            }
+        }
+    }
+}
---
+++ Programming-Basics/ConditionalStatements -Exercise/Metric Converter/Metric Converter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Metric_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number = float.Parse(Console.ReadLine());
+            string merna1 = Console.ReadLine();
+            string merna2 = Console.ReadLine();
+
+            if (merna1 == "m" && merna2 == "mm")
+            {
+                number *= 1000;
+                Console.WriteLine("{0:F3}", number);
+            }
+            else if (merna1 == "mm" && merna2 == "m")
+            {
+                number /= 1000;
+                Console.WriteLine("{0:F3}", number);
+            }
+
+            else if (merna1 == "m" && merna2 == "cm")
+            {
+                number *= 100;
+                Console.WriteLine("{0:F3}", number);
+            }
+            else if (merna1 == "cm" && merna2 == "m")
+            {
+                number /= 100;
+                Console.WriteLine("{0:F3}", number);
+            }
+            else if (merna1 == "cm" && merna2 == "mm")
+            {
+                number *= 10;
+                Console.WriteLine("{0:F3}", number);
+            }
+            else if (merna1 == "mm" && merna2 == "cm")
+            {
+                number /= 10;
+                Console.WriteLine("{0:F3}", number);
+            }
+        }
+    }
+}
