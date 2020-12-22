d3bed27fc54c59361c1c477fb77ecd12cf11da81
Rumen <rbbojilov@gmail.com>
2020-12-22 11:28:23 +0200

    Add exams

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-28and29March2020/Tournament of Christmas/Tournament of Christmas/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Tournament_of_Christmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string text = null;
+            int counterwin = 0;
+            int counterlose = 0;
+            double moneyWin = 0;
+            int counterdaywin = 0;
+            int counterdaylose = 0;
+            double helper = 0;
+            double helper2 = 0;
+            double money = 0;
+            double helper3 = 0;
+            double helper4 = 0;
+            for (int i = 0; i < days; i++)
+            {
+
+
+                while ((text = Console.ReadLine()) != "Finish")
+                {
+
+                    string result = Console.ReadLine();
+
+                    if (result == "win")
+                    {
+                        moneyWin += 20;
+                        counterwin++;
+                        money += 20;
+                    }
+                    else if (result == "lose")
+                    {
+                        counterlose++;
+                    }
+
+
+
+
+                }
+
+                if (counterwin > counterlose)
+                {
+                    helper = moneyWin + (moneyWin * 0.1);
+                    helper2 += helper;
+                    helper3 = money + (money * 0.1);
+                    helper4 += helper3;
+                    counterdaywin++;
+
+                }
+                else
+                {
+                    helper = moneyWin;
+                    helper2 += helper;
+                    helper3 = money;
+                    helper4 += helper3;
+                    counterdaylose++;
+                }
+
+                counterwin = 0;
+                counterlose = 0;
+                moneyWin = 0;
+                money = 0;
+            }
+
+            if (counterdaywin > counterdaylose)
+            {
+
+                helper2 = helper2 + (helper2 * 0.2);
+
+                Console.WriteLine("You won the tournament! Total raised money: {0:F2}", helper2);
+
+            }
+            else
+            {
+                Console.WriteLine("You lost the tournament! Total raised money: {0:F2}", helper4);
+            }
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-28and29March2020/Trekking Mania/Trekking Mania/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Trekking_Mania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broigrupi = int.Parse(Console.ReadLine());
+
+            double broimusala = 0;
+            double broimonblan = 0;
+            double broikiliman = 0;
+            double ke = 0;
+            double everest = 0;
+            double allpeople = 0;
+            for (int i = 0; i < broigrupi; i++)
+            {
+
+                int broihora = int.Parse(Console.ReadLine());
+
+                if (broihora <= 5)
+                {
+
+                    broimusala += broihora;
+
+
+                }
+                else if (broihora > 5 && broihora <= 12)
+                {
+                    broimonblan += broihora;
+                }
+                else if (broihora > 12 && broihora <= 25)
+                {
+                    broikiliman += broihora;
+                }
+                else if (broihora > 25 && broihora <= 40)
+                {
+                    ke += broihora;
+                }
+                else if (broihora > 40)
+                {
+                    everest += broihora;
+                }
+                allpeople += broihora;
+
+            }
+
+
+            broimusala = broimusala / allpeople * 100;
+            broimonblan = broimonblan / allpeople * 100;
+            broikiliman = broikiliman / allpeople * 100;
+            ke = ke / allpeople * 100;
+            everest = everest / allpeople * 100;
+
+            Console.WriteLine("{0:F2}%\n{1:F2}%\n{2:F2}%\n{3:F2}%\n{4:F2}%", broimusala, broimonblan, broikiliman, ke, everest);
+        }
+    }
+}
