35f04acbfc4061a754b82f9a5890d1e5aa272a62
Rumen <rbbojilov@gmail.com>
2020-12-23 11:31:24 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineRetakeExam-2and3May2019/Mobile operator/Mobile operator/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Mobile_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string srok = Console.ReadLine();
+            string type = Console.ReadLine();
+            string internet = Console.ReadLine();
+            int months = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            switch (srok)
+            {
+
+
+                case "one":
+                    {
+
+                        if (type == "Small")
+                        {
+
+                            price = 9.98;
+
+                        }
+                        else if (type == "Middle")
+                        {
+                            price = 18.99;
+                        }
+                        else if (type == "Large")
+                        {
+
+                            price = 25.98;
+
+                        }
+                        else if (type == "ExtraLarge")
+                        {
+                            price = 35.99;
+
+                        }
+
+
+                        if (price <= 10 && internet == "yes")
+                        {
+                            price += 5.50;
+                        }
+                        else if (price <= 30 && price > 10 && internet == "yes")
+                        {
+                            price += 4.35;
+                        }
+                        else if (price > 30 && internet == "yes")
+                        {
+                            price += 3.85;
+                        }
+
+
+
+                        break;
+                    }
+
+
+
+                case "two":
+                    {
+
+                        if (type == "Small")
+                        {
+
+                            price = 8.58;
+
+                        }
+                        else if (type == "Middle")
+                        {
+                            price = 17.09;
+                        }
+                        else if (type == "Large")
+                        {
+
+                            price = 23.59;
+
+                        }
+                        else if (type == "ExtraLarge")
+                        {
+                            price = 31.79;
+
+                        }
+
+
+                        if (price <= 10 && internet == "yes")
+                        {
+                            price += 5.50;
+                        }
+                        else if (price <= 30 && price > 10 && internet == "yes")
+                        {
+                            price += 4.35;
+                        }
+                        else if (price > 30 && internet == "yes")
+                        {
+                            price += 3.85;
+                        }
+
+
+                        price = price - (price * 0.0375);
+
+
+
+                        break;
+                    }
+
+
+
+            }
+
+            Console.WriteLine("{0:F2} lv.", price * months);
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineRetakeExam-2and3May2019/Safari/Safari/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Safari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            double litraGorivo = double.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+
+            double priceFuel = litraGorivo * 2.10;
+            priceFuel += 100;
+
+
+            if (day == "Saturday")
+            {
+                priceFuel = priceFuel - (priceFuel * 0.1);
+            }
+            else if (day == "Sunday")
+            {
+                priceFuel = priceFuel - (priceFuel * 0.2);
+            }
+
+
+            if (priceFuel <= budjet)
+            {
+                Console.WriteLine("Safari time! Money left: {0:F2} lv.", budjet - priceFuel);
+            }
+            else
+            {
+                Console.WriteLine("Not enough money! Money needed: {0:F2} lv.", priceFuel - budjet);
+            }
+        }
+    }
+}
