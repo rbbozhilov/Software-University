fbb057e7d422daf04a318221519e6eee136497af
Rumen <rbbojilov@gmail.com>
2020-12-23 11:33:28 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineRetakeExam-2and3May2019/DivisionWithoutRemainder/Division Without Remainder/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Division_Without_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+
+
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+
+                int input = int.Parse(Console.ReadLine());
+
+                if (input % 2 == 0)
+                {
+                    p1++;
+                }
+                if (input % 3 == 0)
+                {
+                    p2++;
+                }
+                if (input % 4 == 0)
+                {
+                    p3++;
+                }
+
+
+            }
+
+            double percentP1 = 0;
+            double percentp2 = 0;
+            double percentp3 = 0;
+
+
+            percentP1 = (double)p1 / (double)n;
+            percentp2 = (double)p2 / (double)n;
+            percentp3 = (double)p3 / (double)n;
+
+
+            Console.WriteLine("{0:F2}%\n{1:F2}%\n{2:F2}%", percentP1 * 100, percentp2 * 100, percentp3 * 100);
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineRetakeExam-2and3May2019/FoodDelivery/Food Delivery/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Food_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pileMenu = int.Parse(Console.ReadLine());
+            int ribaMenu = int.Parse(Console.ReadLine());
+            int vegaMenu = int.Parse(Console.ReadLine());
+
+
+            double sumPile = (double)pileMenu * 10.35;
+            double sumRiba = (double)ribaMenu * 12.40;
+            double sumVega = (double)vegaMenu * 8.15;
+
+            double totalSum = sumPile + sumRiba + sumVega;
+
+            totalSum = totalSum + (totalSum * 0.2) + 2.50;
+
+
+            Console.WriteLine("Total: {0:F2}", totalSum);
+        }
+    }
+}
