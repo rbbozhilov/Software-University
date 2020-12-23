80f14e00cc39321cb632c0aeacf143e58ceec0ce
Rumen <rbbojilov@gmail.com>
2020-12-23 11:30:12 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineRetakeExam-2and3May2019/Tourist Shop/Tourist Shop/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Tourist_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counterProducts = 0;
+
+            double budjet = double.Parse(Console.ReadLine());
+
+            string text = string.Empty;
+            bool flag = false;
+            double sum = 0;
+
+            while ((text = Console.ReadLine()) != "Stop")
+            {
+
+                double price = double.Parse(Console.ReadLine());
+
+                counterProducts++;
+
+
+                if (counterProducts % 3 == 0)
+                {
+
+                    price = price / 2.0;
+
+
+                    sum += price;
+                    budjet -= price;
+
+
+                }
+                else
+                {
+                    sum += price;
+                    budjet -= price;
+                }
+
+
+
+                if (budjet < 0)
+                {
+
+                    flag = true;
+
+
+
+
+                    break;
+
+
+                }
+
+            }
+
+
+            if (flag)
+            {
+
+                Console.WriteLine("You don't have enough money!\nYou need {0:F2} leva!", Math.Abs(budjet));
+
+            }
+            else
+            {
+                Console.WriteLine("You bought {0} products for {1:F2} leva.", counterProducts, sum);
+            }
+
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineRetakeExam-2and3May2019/Vet Parking/Vet Parking/Food for Pets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Food_for_Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var days = int.Parse(Console.ReadLine());
+            var totalFood = double.Parse(Console.ReadLine());
+
+            var catFood = 0.0;
+            var dogFood = 0.0;
+            double biscuits = 0;
+
+            var eatenFoodByCat = 0.0;
+            var eatenFoodByDog = 0.0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                dogFood = int.Parse(Console.ReadLine());
+                catFood = int.Parse(Console.ReadLine());
+
+
+                eatenFoodByCat += catFood;
+                eatenFoodByDog += dogFood;
+
+
+                if (i % 3 == 0)
+                {
+                    biscuits += (dogFood + catFood) * 0.1;
+                }
+
+
+
+            }
+
+            var sum = eatenFoodByCat + eatenFoodByDog;
+            var eatenFood = sum / totalFood * 100;
+            var eatenFromDog = eatenFoodByDog / sum * 100;
+            var eatenFromCat = eatenFoodByCat / sum * 100;
+
+            Console.WriteLine($"Total eaten biscuits: {Math.Round(biscuits)}gr.");
+            Console.WriteLine($"{eatenFood:f2}% of the food has been eaten.");
+            Console.WriteLine($"{eatenFromDog:f2}% eaten from the dog.");
+            Console.WriteLine($"{eatenFromCat:f2}% eaten from the cat.");
+        }
+    }
+}
