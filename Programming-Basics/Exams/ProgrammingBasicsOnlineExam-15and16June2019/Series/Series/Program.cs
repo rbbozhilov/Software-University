c87b3e15ef7dba7db80268ed9c4a94d277b75130
Rumen <rbbojilov@gmail.com>
2020-12-24 10:38:43 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-15and16June2019/Series/Series/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            int broi = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = 0; i < broi; i++)
+            {
+
+                string name = Console.ReadLine();
+                double cena = double.Parse(Console.ReadLine());
+
+                if (name == "Thrones")
+                {
+                    cena = cena - (cena * 0.5);
+
+                    sum += cena;
+
+                }
+                else if (name == "Lucifer")
+                {
+                    cena = cena - (cena * 0.4);
+                    sum += cena;
+                }
+                else if (name == "Protector")
+                {
+                    cena = cena - (cena * 0.3);
+                    sum += cena;
+                }
+                else if (name == "TotalDrama")
+                {
+                    cena = cena - (cena * 0.2);
+                    sum += cena;
+                }
+                else if (name == "Area")
+                {
+                    cena = cena - (cena * 0.1);
+                    sum += cena;
+                }
+                else
+                {
+                    sum += cena;
+                }
+
+            }
+
+
+            if (budjet >= sum)
+            {
+                Console.WriteLine("You bought all the series and left with {0:F2} lv.", budjet - sum);
+            }
+            else
+            {
+                Console.WriteLine("You need {0:F2} lv. more to buy the series!", Math.Abs(budjet - sum));
+            }
+
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-15and16June2019/SeriesCalculator/Series Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Series_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int seasons = int.Parse(Console.ReadLine());
+            int episodes = int.Parse(Console.ReadLine());
+            double episodeTime = double.Parse(Console.ReadLine());
+
+            double produljitelnost = episodeTime + (episodeTime * 0.2);
+            double produljitelnostSpecialno = seasons * 10;
+
+            double finalResult = episodes * produljitelnost * seasons + produljitelnostSpecialno;
+
+
+            Console.WriteLine("Total time needed to watch the {0} series is {1} minutes.", name, Math.Floor(finalResult));
+        }
+    }
+}
