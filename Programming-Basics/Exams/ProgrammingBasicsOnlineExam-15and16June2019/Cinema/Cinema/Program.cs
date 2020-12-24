4c311485aa8e7bc1f33cd6abfca84fe2eb2e541f
Rumen <rbbojilov@gmail.com>
2020-12-24 10:42:53 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-15and16June2019/Cinema/Cinema/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kapacitet = int.Parse(Console.ReadLine());
+            string text = null;
+            int sumPeople = 0;
+            int sum = 0;
+            bool checker = false;
+            while ((text = Console.ReadLine()) != "Movie time!")
+            {
+
+                int broiHora = int.Parse(text);
+                sumPeople += broiHora;
+
+                if (sumPeople > kapacitet)
+                {
+                    Console.WriteLine("The cinema is full.");
+                    checker = true;
+                    break;
+                }
+
+
+                if (broiHora % 3 == 0)
+                {
+
+                    sum += (broiHora * 5) - 5;
+
+
+                }
+                else
+                {
+                    sum += broiHora * 5;
+                }
+            }
+
+
+            if (checker == true)
+            {
+
+                Console.WriteLine("Cinema income - {0} lv.", sum);
+            }
+            else
+            {
+
+                sumPeople = kapacitet - sumPeople;
+                Console.WriteLine("There are {0} seats left in the cinema.\nCinema income - {1} lv.", sumPeople, sum);
+
+            }
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-15and16June2019/Favorite Movie/Favorite Movie/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Favorite_Movie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int currentLetter = 0;
+            int maxPoints = 0;
+            string bestMovie = null;
+            int i = 0;
+
+
+            for (i = 0; i < 7; i++)
+            {
+
+
+
+
+                string name = Console.ReadLine();
+                if (name == "STOP")
+                {
+                    break;
+                }
+                int length = name.Length;
+
+                for (int j = 0; j < name.Length; j++)
+                {
+
+                    currentLetter = (int)name[j];
+
+
+                    if (currentLetter >= 65 && currentLetter <= 90)
+                    {
+
+                        sum += currentLetter - length;
+
+
+                    }
+                    else if (currentLetter >= 97 && currentLetter <= 122)
+                    {
+
+
+                        sum += currentLetter - (2 * length);
+
+
+                    }
+                    else
+                    {
+                        sum += currentLetter;
+                    }
+
+
+
+
+
+                }
+
+                if (sum > maxPoints)
+                {
+                    maxPoints = sum;
+                    bestMovie = name;
+                }
+                sum = 0;
+
+
+            }
+
+            if (i == 7)
+            {
+
+                Console.WriteLine("The limit is reached.\nThe best movie for you is {0} with {1} ASCII sum.", bestMovie, maxPoints);
+            }
+
+            else
+            {
+                Console.WriteLine("The best movie for you is {0} with {1} ASCII sum.", bestMovie, maxPoints);
+            }
+        }
+    }
+}
