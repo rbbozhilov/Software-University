c797e7db458b3473bb476fe82b76cd2d5b2a5b26
Rumen <rbbojilov@gmail.com>
2020-12-24 10:42:02 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-15and16June2019/Film Premiere/Film Premiere/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace Film_Premiere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string packet = Console.ReadLine();
+            int broiTickets = int.Parse(Console.ReadLine());
+            double cena = 0;
+
+
+
+            switch (packet)
+            {
+
+                case "Drink":
+                    {
+
+                        if (name == "John Wick")
+                        {
+
+                            cena = 12 * broiTickets;
+
+                        }
+                        else if (name == "Star Wars")
+                        {
+
+                            cena = 18 * broiTickets;
+                            if (broiTickets >= 4)
+                            {
+
+                                cena = cena - (cena * 0.3);
+
+                            }
+                        }
+
+                        else if (name == "Jumanji")
+                        {
+
+
+                            cena = 9 * broiTickets;
+
+                            if (broiTickets == 2)
+                            {
+                                cena = cena - (cena * 0.15);
+                            }
+
+                        }
+
+
+                        break;
+                    }
+
+                case "Popcorn":
+                    {
+
+                        if (name == "John Wick")
+                        {
+
+                            cena = 15 * broiTickets;
+
+                        }
+                        else if (name == "Star Wars")
+                        {
+
+                            cena = 25 * broiTickets;
+                            if (broiTickets >= 4)
+                            {
+
+                                cena = cena - (cena * 0.3);
+
+                            }
+                        }
+
+                        else if (name == "Jumanji")
+                        {
+
+
+                            cena = 11 * broiTickets;
+
+                            if (broiTickets == 2)
+                            {
+                                cena = cena - (cena * 0.15);
+                            }
+
+                        }
+
+                        break;
+
+
+
+                    }
+
+
+                case "Menu":
+                    {
+
+
+
+                        if (name == "John Wick")
+                        {
+
+                            cena = 19 * broiTickets;
+
+                        }
+                        else if (name == "Star Wars")
+                        {
+
+                            cena = 30 * broiTickets;
+                            if (broiTickets >= 4)
+                            {
+
+                                cena = cena - (cena * 0.3);
+
+                            }
+                        }
+
+                        else if (name == "Jumanji")
+                        {
+
+
+                            cena = 14 * broiTickets;
+
+                            if (broiTickets == 2)
+                            {
+                                cena = cena - (cena * 0.15);
+                            }
+
+                        }
+
+                        break;
+                    }
+
+
+            }
+
+
+            Console.WriteLine("Your bill is {0:F2} leva.", cena);
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-15and16June2019/Lunch Break/Lunch Break/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lunch_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int produljitelnostserial = int.Parse(Console.ReadLine());
+            int produljitelnostpochivka = int.Parse(Console.ReadLine());
+
+
+
+            double vremeobed = (double)produljitelnostpochivka * 1 / 8;
+
+            double vremeotdih = (double)produljitelnostpochivka * 1 / 4;
+
+            double otherTime = produljitelnostpochivka - vremeobed - vremeotdih;
+
+
+            if (otherTime >= produljitelnostserial)
+            {
+                otherTime = otherTime - produljitelnostserial;
+                Console.WriteLine("You have enough time to watch {0} and left with {1} minutes free time.", name, Math.Ceiling(otherTime));
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough time to watch {0}, you need {1} more minutes.", name, Math.Ceiling(Math.Abs(otherTime - produljitelnostserial)));
+            }
+        }
+    }
+}
