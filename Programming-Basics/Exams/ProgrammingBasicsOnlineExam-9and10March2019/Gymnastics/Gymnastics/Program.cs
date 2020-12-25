e6c2e31bea404d81926cf76069a98ecd721586dd
Rumen <rbbojilov@gmail.com>
2020-12-25 16:27:56 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-9and10March2019/Gymnastics/Gymnastics/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Gymnastics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string ured = Console.ReadLine();
+
+            double mark = 0;
+
+            switch (ured)
+            {
+
+                case "ribbon":
+                    {
+
+
+                        if (country == "Bulgaria")
+                        {
+
+                            mark = 9.600 + 9.400;
+
+
+                        }
+
+
+                        else if (country == "Russia")
+                        {
+
+                            mark = 9.100 + 9.400;
+                        }
+
+                        else if (country == "Italy")
+                        {
+                            mark = 9.200 + 9.500;
+                        }
+
+
+                        break;
+                    }
+
+                case "hoop":
+                    {
+
+
+                        if (country == "Bulgaria")
+                        {
+
+                            mark = 9.550 + 9.750;
+
+
+                        }
+
+
+                        else if (country == "Russia")
+                        {
+
+                            mark = 9.300 + 9.800;
+                        }
+
+                        else if (country == "Italy")
+                        {
+                            mark = 9.450 + 9.350;
+                        }
+
+
+                        break;
+                    }
+
+
+
+                case "rope":
+                    {
+
+
+                        if (country == "Bulgaria")
+                        {
+
+                            mark = 9.500 + 9.400;
+
+
+                        }
+
+
+                        else if (country == "Russia")
+                        {
+
+                            mark = 9.600 + 9.000;
+                        }
+
+                        else if (country == "Italy")
+                        {
+                            mark = 9.700 + 9.150;
+                        }
+
+
+                        break;
+                    }
+
+
+
+
+            }
+
+
+            double percent = ((20 - mark) / 20) * 100;
+
+
+            Console.WriteLine("The team of {3} get {0:F3} on {1}.\n{2:F2}%", mark, ured, percent, country);
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-9and10March2019/High Jump/High Jump/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace High_Jump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wantedHeight = int.Parse(Console.ReadLine());
+            int currentHeight = wantedHeight - 30;
+            int counter = 0;
+            bool flag = false;
+            int loseHeight = 0;
+            while (true)
+            {
+
+
+                int jumpHeigh = int.Parse(Console.ReadLine());
+                counter++;
+
+                if (jumpHeigh > currentHeight)
+                {
+
+                    currentHeight += 5;
+
+                    flag = true;
+
+                }
+
+                else
+                {
+
+                    for (int i = 0; i < 2; i++)
+                    {
+
+                        jumpHeigh = int.Parse(Console.ReadLine());
+                        counter++;
+
+                        if (jumpHeigh > currentHeight)
+                        {
+                            currentHeight += 5;
+
+                            flag = true;
+                            break;
+                        }
+                        else
+                        {
+
+                            flag = false;
+                            loseHeight = currentHeight;
+
+                        }
+
+
+                    }
+
+
+                }
+
+
+                if (!flag)
+                {
+                    break;
+                }
+
+
+                if (currentHeight > wantedHeight)
+                {
+                    break;
+                }
+
+
+            }
+
+
+
+            if (flag)
+            {
+
+
+                Console.WriteLine("Tihomir succeeded, he jumped over {0}cm after {1} jumps.", wantedHeight, counter);
+
+            }
+            else
+            {
+                Console.WriteLine("Tihomir failed at {0}cm after {1} jumps.", loseHeight, counter);
+            }
+        }
+    }
+}
