5e907e3d30591fbb64033c393eda7aeb13bd39c7
Rumen <rbbojilov@gmail.com>
2020-12-25 16:26:14 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-9and10March2019/Skeleton/Skeleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Skeleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesControl = int.Parse(Console.ReadLine());
+            int secondsControl = int.Parse(Console.ReadLine());
+            double ulei = double.Parse(Console.ReadLine());
+            int secondsForHundred = int.Parse(Console.ReadLine());
+
+
+            int sumControl = minutesControl * 60 + secondsControl;
+
+            double timeLose = (double)ulei / 120;
+
+
+            double totalTimelose = (double)timeLose * 2.5;
+
+            double timeOfMarin = (double)((ulei / 100) * secondsForHundred - totalTimelose);
+
+            if (timeOfMarin <= sumControl)
+            {
+                Console.WriteLine("Marin Bangiev won an Olympic quota!\nHis time is {0:F3}.", timeOfMarin);
+            }
+            else
+            {
+
+                Console.WriteLine("No, Marin failed! He was {0:F3} second slower.", Math.Abs(timeOfMarin - sumControl));
+
+
+            }
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-9and10March2019/Tennis Equipment/Tennis Equipment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tennis_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceOneRacket = double.Parse(Console.ReadLine());
+            int countRackets = int.Parse(Console.ReadLine());
+            int countShoes = int.Parse(Console.ReadLine());
+
+
+            double priceRacket = priceOneRacket * countRackets;
+
+            double priceShoes = (double)priceOneRacket / 6;
+
+            double priceAllShoes = priceShoes * countShoes;
+
+            double priceOther = (priceRacket + priceAllShoes) * 0.2;
+
+            double finalPrice = priceRacket + priceOther + priceAllShoes;
+
+
+
+
+            Console.WriteLine("Price to be paid by Djokovic {0}", Math.Floor(finalPrice / 8));
+            Console.WriteLine("Price to be paid by sponsors {0}", Math.Ceiling(finalPrice * 7 / 8));
+        }
+    }
+}
