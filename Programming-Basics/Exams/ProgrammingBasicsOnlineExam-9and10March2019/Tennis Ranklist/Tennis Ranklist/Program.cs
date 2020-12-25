0d5859591ae7651ff4c32ea1ba5e6e433f2d4e67
Rumen <rbbojilov@gmail.com>
2020-12-25 16:24:46 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-9and10March2019/Tennis Ranklist/Tennis Ranklist/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Tennis_Ranklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tournaments = int.Parse(Console.ReadLine());
+            int startPoints = int.Parse(Console.ReadLine());
+
+            int counterWon = 0;
+            double average = 0;
+            double percent = 0;
+
+            for (int i = 0; i < tournaments; i++)
+            {
+
+                string etap = Console.ReadLine();
+
+
+                if (etap == "W")
+                {
+                    counterWon++;
+
+                    startPoints += 2000;
+                    average += 2000;
+
+                }
+                else if (etap == "F")
+                {
+                    startPoints += 1200;
+                    average += 1200;
+                }
+                else if (etap == "SF")
+                {
+                    startPoints += 720;
+                    average += 720;
+                }
+
+
+            }
+
+
+            average = Math.Floor((double)average / tournaments);
+
+            percent = (double)counterWon / tournaments * 100;
+
+
+            Console.WriteLine("Final points: {0}\nAverage points: {1}\n{2:F2}%", startPoints, average, percent);
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-9and10March2019/World Snooker Championship/World Snooker Championship/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace World_Snooker_Championship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string etap = Console.ReadLine();
+            string typeTicket = Console.ReadLine();
+            int tickets = int.Parse(Console.ReadLine());
+            string picture = Console.ReadLine();
+            double Price = 0;
+
+            switch (typeTicket)
+            {
+
+                case "Standard":
+                    {
+
+                        if (etap == "Quarter final")
+                        {
+                            Price = 55.50;
+
+
+                        }
+
+                        else if (etap == "Semi final")
+                        {
+                            Price = 75.88;
+                        }
+                        else if (etap == "Final")
+                        {
+                            Price = 110.10;
+                        }
+
+
+                        break;
+                    }
+
+
+                case "Premium":
+                    {
+
+                        if (etap == "Quarter final")
+                        {
+                            Price = 105.20;
+
+
+                        }
+
+                        else if (etap == "Semi final")
+                        {
+                            Price = 125.22;
+                        }
+                        else if (etap == "Final")
+                        {
+                            Price = 160.66;
+                        }
+
+
+
+                        break;
+                    }
+
+
+
+                case "VIP":
+                    {
+
+                        if (etap == "Quarter final")
+                        {
+                            Price = 118.90;
+
+
+                        }
+
+                        else if (etap == "Semi final")
+                        {
+                            Price = 300.40;
+                        }
+                        else if (etap == "Final")
+                        {
+                            Price = 400;
+                        }
+
+
+
+                        break;
+                    }
+
+
+
+            }
+
+
+            double totalPriceTickets = tickets * Price;
+
+            if (totalPriceTickets > 4000)
+            {
+                totalPriceTickets = totalPriceTickets - (totalPriceTickets * 0.25);
+            }
+            else if (totalPriceTickets > 2500 && totalPriceTickets <= 4000)
+            {
+
+
+                totalPriceTickets = totalPriceTickets - (totalPriceTickets * 0.1);
+
+
+                if (picture == "Y")
+                {
+                    totalPriceTickets += tickets * 40;
+                }
+
+            }
+
+            else
+            {
+
+                if (picture == "Y")
+                {
+
+                    totalPriceTickets += tickets * 40;
+                }
+
+
+            }
+
+
+
+            Console.WriteLine("{0:F2}", totalPriceTickets);
+        }
+    }
+}
