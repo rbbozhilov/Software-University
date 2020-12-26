30288930cdd7df6ac5bddb7ae209406382c403ab
Rumen <rbbojilov@gmail.com>
2020-12-26 11:34:42 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-9and10March2019/Darts/Darts/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Darts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int totalPoints = 301;
+            int counterNotSuccess = 0;
+            int counterSuccess = 0;
+
+            string text = null;
+
+
+            while ((text = Console.ReadLine()) != "Retire")
+            {
+
+                int points = int.Parse(Console.ReadLine());
+
+
+                if (text == "Triple")
+                {
+
+
+                    if (totalPoints >= points * 3)
+                    {
+                        totalPoints -= points * 3;
+                        counterSuccess++;
+                    }
+                    else
+                    {
+                        counterNotSuccess++;
+                    }
+                }
+                else if (text == "Double")
+                {
+
+                    if (totalPoints >= points * 2)
+                    {
+                        totalPoints -= points * 2;
+                        counterSuccess++;
+                    }
+                    else
+                    {
+                        counterNotSuccess++;
+                    }
+
+                }
+                else if (text == "Single")
+                {
+                    if (totalPoints >= points)
+                    {
+                        totalPoints -= points;
+                        counterSuccess++;
+                    }
+                    else
+                    {
+                        counterNotSuccess++;
+                    }
+                }
+
+                if (totalPoints == 0)
+                {
+                    break;
+                }
+
+            }
+
+
+
+            if (totalPoints > 0)
+            {
+                Console.WriteLine("{0} retired after {1} unsuccessful shots.", name, counterNotSuccess);
+            }
+            else
+            {
+                Console.WriteLine("{0} won the leg with {1} shots.", name, counterSuccess);
+            }
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-9and10March2019/Fitness Center/Fitness Center/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Fitness_Center
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fitnesMembers = int.Parse(Console.ReadLine());
+
+            int counterChest = 0;
+            int counterBack = 0;
+            int counterLegs = 0;
+            int counterAbs = 0;
+            int counterProteinShake = 0;
+            int counterProteinBar = 0;
+            int counterTrained = 0;
+            double percentWork = 0;
+            double percentProtein = 0;
+
+            for (int i = 0; i < fitnesMembers; i++)
+            {
+
+
+                string text = Console.ReadLine();
+
+
+                if (text == "Back")
+                {
+
+                    counterBack++;
+                    counterTrained++;
+
+                }
+                else if (text == "Chest")
+                {
+                    counterChest++;
+                    counterTrained++;
+                }
+                else if (text == "Abs")
+                {
+                    counterAbs++;
+                    counterTrained++;
+                }
+                else if (text == "Legs")
+                {
+                    counterLegs++;
+                    counterTrained++;
+                }
+                else if (text == "Protein shake")
+                {
+                    counterProteinShake++;
+                }
+                else if (text == "Protein bar")
+                {
+                    counterProteinBar++;
+                }
+
+
+
+
+            }
+
+
+            percentWork = (double)counterTrained / fitnesMembers * 100;
+            percentProtein = (double)(counterProteinBar + counterProteinShake) / fitnesMembers * 100;
+
+
+            Console.WriteLine("{0} - back\n{1} - chest\n{2} - legs\n{3} - abs\n{4} - protein shake\n{5} - protein bar\n{6:F2}% - work out\n{7:F2}% - protein", counterBack, counterChest, counterLegs, counterAbs, counterProteinShake, counterProteinBar, percentWork, percentProtein);
+        }
+    }
+}
