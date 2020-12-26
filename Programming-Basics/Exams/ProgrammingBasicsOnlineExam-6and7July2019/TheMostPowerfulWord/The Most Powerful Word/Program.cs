cdc9b954c1ae1872de4bb5e5ce118d900000a4c8
Rumen <rbbojilov@gmail.com>
2020-12-26 11:38:30 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/TheMostPowerfulWord/The Most Powerful Word/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace The_Most_Powerful_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = null;
+            double sum = 0;
+            double points = 0;
+            double maxPoints = Int32.MinValue;
+            string helper = null;
+            while ((text = Console.ReadLine()) != "End of words")
+            {
+
+                int length = text.Length;
+
+                for (int i = 0; i < length; i++)
+                {
+
+                    sum += (int)text[i];
+                    points += (int)text[i];
+
+                }
+
+                if (text[0] == 'o' || text[0] == 'a' || text[0] == 'e' || text[0] == 'i' || text[0] == 'u' || text[0] == 'y' || text[0] == 'A' || text[0] == 'O' || text[0] == 'U' || text[0] == 'E' || text[0] == 'I' || text[0] == 'Y')
+                {
+
+                    points *= length;
+
+
+                }
+                else
+                {
+                    points /= length;
+                    points = Math.Floor(points);
+                }
+
+                if (points > maxPoints)
+                {
+                    maxPoints = points;
+                    helper = text;
+                }
+
+
+
+
+
+                points = 0;
+
+            }
+
+
+
+            Console.WriteLine("The most powerful word is {0} - {1}", helper, maxPoints);
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/TravelAgency/Travel Agency/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+namespace Travel_Agency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            string packet = Console.ReadLine();
+            string otstupka = Console.ReadLine();
+            int dni = int.Parse(Console.ReadLine());
+            double cena = 0;
+            double obshto = 0;
+            if (dni < 1)
+            {
+                Console.WriteLine("Days must be positive number!");
+                System.Environment.Exit(1);
+            }
+
+
+            if ((packet == "noEquipment" || packet == "withEquipment" || packet == "noBreakfast" || packet == "withBreakfast") && (town == "Bansko" || town == "Borovets" || town == "Varna" || town == "Burgas"))
+            {
+
+
+                switch (town)
+                {
+
+                    case "Bansko":
+                        {
+
+                            if (packet == "noEquipment")
+                            {
+
+                                cena = 80;
+
+                                if (otstupka == "yes")
+                                {
+                                    cena = cena - (cena * 0.05);
+                                }
+
+
+
+
+                            }
+                            else if (packet == "withEquipment")
+                            {
+                                cena = 100;
+
+                                if (otstupka == "yes")
+                                {
+                                    cena = cena - (cena * 0.1);
+
+
+                                }
+
+
+                            }
+
+                            if (dni > 7)
+                            {
+                                dni--;
+                            }
+
+                            obshto = cena * dni;
+
+
+
+                            break;
+                        }
+
+                    case "Borovets":
+                        {
+
+                            if (packet == "noEquipment")
+                            {
+
+                                cena = 80;
+
+                                if (otstupka == "yes")
+                                {
+                                    cena = cena - (cena * 0.05);
+                                }
+
+
+
+
+                            }
+                            else if (packet == "withEquipment")
+                            {
+                                cena = 100;
+
+                                if (otstupka == "yes")
+                                {
+                                    cena = cena - (cena * 0.1);
+
+
+                                }
+
+
+                            }
+
+                            if (dni > 7)
+                            {
+                                dni--;
+                            }
+
+                            obshto = cena * dni;
+
+
+
+                            break;
+                        }
+
+
+                    case "Varna":
+                        {
+
+                            if (packet == "withBreakfast")
+                            {
+
+                                cena = 130;
+
+                                if (otstupka == "yes")
+                                {
+                                    cena = cena - (cena * 0.12);
+                                }
+
+
+
+
+                            }
+                            else if (packet == "noBreakfast")
+                            {
+                                cena = 100;
+
+                                if (otstupka == "yes")
+                                {
+                                    cena = cena - (cena * 0.07);
+
+
+                                }
+
+
+                            }
+
+                            if (dni > 7)
+                            {
+                                dni--;
+                            }
+
+                            obshto = cena * dni;
+
+
+
+                            break;
+                        }
+
+                    case "Burgas":
+                        {
+
+                            if (packet == "withBreakfast")
+                            {
+
+                                cena = 130;
+
+                                if (otstupka == "yes")
+                                {
+                                    cena = cena - (cena * 0.12);
+                                }
+
+
+
+
+                            }
+                            else if (packet == "noBreakfast")
+                            {
+                                cena = 100;
+
+                                if (otstupka == "yes")
+                                {
+                                    cena = cena - (cena * 0.07);
+
+
+                                }
+
+
+                            }
+
+                            if (dni > 7)
+                            {
+                                dni--;
+                            }
+
+                            obshto = cena * dni;
+
+
+
+                            break;
+                        }
+
+                }
+
+                Console.WriteLine("The price is {0:F2}lv! Have a nice time!", obshto);
+
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
