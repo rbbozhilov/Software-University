70e6953ec4da7c6836fd7741f0717a6e6e327855
Rumen <rbbojilov@gmail.com>
2020-12-26 11:41:00 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/PoolDay/Pool Day/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pool_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broihora = int.Parse(Console.ReadLine());
+            double taksa = double.Parse(Console.ReadLine());
+            double cenashezlong = double.Parse(Console.ReadLine());
+            double cenachadur = double.Parse(Console.ReadLine());
+
+
+            double taksavhod = taksa * broihora;
+            double broishazlongi = broihora * 0.75;
+            broishazlongi = Math.Ceiling(broishazlongi);
+            double obshtocenashazlongi = broishazlongi * cenashezlong;
+            double broichaduri = broihora * 0.5;
+            broichaduri = Math.Ceiling(broichaduri);
+            double obshtacenachaduri = broichaduri * cenachadur;
+
+            double finalSum = taksavhod + obshtocenashazlongi + obshtacenachaduri;
+
+            Console.WriteLine("{0:F2} lv.", finalSum);
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/Renovation/Renovation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Renovation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heightWall = int.Parse(Console.ReadLine());
+            int weightWall = int.Parse(Console.ReadLine());
+            int percentWall = int.Parse(Console.ReadLine());
+
+
+            double total = heightWall * weightWall * 4;
+            double painedWalls = (double)percentWall / 100.0;
+            painedWalls = total - (total * painedWalls);
+
+            painedWalls = Math.Ceiling(painedWalls);
+
+            string text = string.Empty;
+            bool flag = false;
+            while ((text = Console.ReadLine()) != "Tired!")
+            {
+
+
+                int boia = int.Parse(text);
+
+                painedWalls -= boia;
+
+                if (painedWalls < 0)
+                {
+                    Console.WriteLine("All walls are painted and you have {0} l paint left!", Math.Abs(painedWalls));
+                    Environment.Exit(0);
+                }
+                else if (painedWalls == 0)
+                {
+                    Console.WriteLine("All walls are painted! Great job, Pesho!");
+
+                    Environment.Exit(0);
+                }
+
+
+            }
+
+            if (flag == false)
+            {
+                Console.WriteLine("{0} quadratic m left.", painedWalls);
+            }
+
+        }
+    }
+}
