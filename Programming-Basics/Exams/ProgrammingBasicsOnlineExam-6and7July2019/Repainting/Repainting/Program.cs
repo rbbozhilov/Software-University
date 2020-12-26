0cccba510f31a3f06c1d19e39b59be118b8cbf52
Rumen <rbbojilov@gmail.com>
2020-12-26 11:39:33 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/Repainting/Repainting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nailon = int.Parse(Console.ReadLine());
+            int boia = int.Parse(Console.ReadLine());
+            int razreditel = int.Parse(Console.ReadLine());
+            int chasove = int.Parse(Console.ReadLine());
+
+
+            double kolichestvoNailon = (double)(nailon + 2) * 1.50;
+
+            double neobhodimoBoia = ((double)boia + ((double)boia * 0.1)) * 14.50;
+
+
+
+            double razreditelNeobhodim = razreditel * 5;
+
+            double finalSum = kolichestvoNailon + neobhodimoBoia + razreditelNeobhodim + 0.40;
+
+
+            double payWorkers = (finalSum * 0.3) * chasove;
+
+            double totalSum = finalSum + payWorkers;
+
+            Console.WriteLine("Total expenses: {0:F2} lv.", totalSum);
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/Shopping/Shopping/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            int videoCards = int.Parse(Console.ReadLine());
+            int procesors = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+
+
+
+            double sumVideoCards = 250 * videoCards;
+            double sumProcessor = (sumVideoCards * 0.35) * procesors;
+            double sumRam = (sumVideoCards * 0.1) * ram;
+
+
+            double finalSum = sumVideoCards + sumProcessor + sumRam;
+
+            if (videoCards > procesors)
+            {
+                finalSum = finalSum - (finalSum * 0.15);
+            }
+
+
+            if (finalSum <= budjet)
+            {
+                Console.WriteLine("You have {0:F2} leva left!", budjet - finalSum);
+            }
+            else
+            {
+                Console.WriteLine("Not enough money! You need {0:F2} leva more!", finalSum - budjet);
+            }
+        }
+    }
+}
