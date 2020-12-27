826a4c4e27a7d007a5412f7a2ed165edc1480d00
Rumen <rbbojilov@gmail.com>
2020-12-27 10:35:43 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/Coffee Machine/Coffee Machine/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Coffee_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string sugar = Console.ReadLine();
+            int totalDrinks = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            switch (drink)
+            {
+
+                case "Espresso":
+                    {
+
+                        if (sugar == "Without")
+                        {
+                            price = 0.90 * totalDrinks;
+
+                            price = price - (price * 0.35);
+
+
+
+                        }
+                        else if (sugar == "Normal")
+                        {
+                            price = 1 * totalDrinks;
+                        }
+                        else if (sugar == "Extra")
+                        {
+                            price = 1.20 * totalDrinks;
+
+                        }
+
+
+                        if (totalDrinks >= 5)
+                        {
+                            price = price - (price * 0.25);
+                        }
+
+                        break;
+                    }
+
+                case "Cappuccino":
+                    {
+                        if (sugar == "Without")
+                        {
+                            price = 1 * totalDrinks;
+                            price = price - (price * 0.35);
+                        }
+                        else if (sugar == "Normal")
+                        {
+                            price = 1.20 * totalDrinks;
+                        }
+                        else if (sugar == "Extra")
+                        {
+                            price = 1.60 * totalDrinks;
+
+                        }
+                        break;
+                    }
+
+
+
+                case "Tea":
+                    {
+                        if (sugar == "Without")
+                        {
+                            price = 0.50 * totalDrinks;
+                            price = price - (price * 0.35);
+                        }
+                        else if (sugar == "Normal")
+                        {
+                            price = 0.60 * totalDrinks;
+                        }
+                        else if (sugar == "Extra")
+                        {
+                            price = 0.70 * totalDrinks;
+
+                        }
+                        break;
+                    }
+
+
+            }
+
+
+
+            if (price > 15)
+            {
+                price = price - (price * 0.2);
+            }
+
+
+            Console.WriteLine("You bought {0} cups of {1} for {2:F2} lv.", totalDrinks, drink, price);
+
+        }
+    }
+}
