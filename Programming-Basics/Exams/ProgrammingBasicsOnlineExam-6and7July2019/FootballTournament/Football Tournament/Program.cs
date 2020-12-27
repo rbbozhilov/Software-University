7a00e0851ce1a38bc9967cb06d32b12fef1e3071
Rumen <rbbojilov@gmail.com>
2020-12-27 10:32:48 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/FootballTournament/Football Tournament/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Football_Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string teamName = Console.ReadLine();
+            int games = int.Parse(Console.ReadLine());
+            int counterWin = 0;
+            int counterLose = 0;
+            int counterDraw = 0;
+
+
+            if (games <= 0)
+            {
+                Console.WriteLine("{0} hasn't played any games during this season.", teamName);
+            }
+            else
+            {
+
+                for (int i = 0; i < games; i++)
+                {
+
+                    string result = Console.ReadLine();
+
+
+                    if (result == "W")
+                    {
+                        counterWin++;
+                    }
+                    else if (result == "L")
+                    {
+                        counterLose++;
+                    }
+                    else if (result == "D")
+                    {
+                        counterDraw++;
+                    }
+
+
+
+                }
+
+                int totalPoints = (counterWin * 3) + (counterDraw * 1);
+
+                double percentWin = ((double)counterWin / (double)games) * 100.0;
+
+
+
+                Console.WriteLine("{0} has won {1} points during this season.\nTotal stats:\n## W: {2}\n## D: {3}\n## L: {4}\nWin rate: {5:F2}%", teamName, totalPoints, counterWin, counterDraw, counterLose, percentWin);
+
+
+
+            }
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7July2019/NameGame/Name Game/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Name_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            string winnerName = string.Empty;
+            int currentPoints = 0;
+            int winnerPoints = int.MinValue;
+
+
+            while ((name = Console.ReadLine()) != "Stop")
+            {
+
+
+                for (int i = 0; i < name.Length; i++)
+                {
+
+                    int number = int.Parse(Console.ReadLine());
+
+                    if (number == (int)name[i])
+                    {
+
+                        currentPoints += 10;
+
+                    }
+                    else
+                    {
+                        currentPoints += 2;
+                    }
+
+
+
+
+                }
+
+
+                if (currentPoints >= winnerPoints)
+                {
+                    winnerPoints = currentPoints;
+                    winnerName = name;
+                }
+
+                currentPoints = 0;
+            }
+
+
+            Console.WriteLine("The winner is {0} with {1} points!", winnerName, winnerPoints);
+        }
+    }
+}
