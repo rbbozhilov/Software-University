bde9d0a52b9159842baa9e2d226f2f94c6ecf8ba
Rumen <rbbojilov@gmail.com>
2020-12-28 11:30:59 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7April2019/Cinema Tickets/Cinema Tickets/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Cinema_Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameMovie = null;
+            double percentFullHall = 0;
+            int totalTickets = 0;
+            double percentStudent = 0;
+            double percentStandard = 0;
+            double percentKid = 0;
+            int counter = 0;
+
+
+            while ((nameMovie = Console.ReadLine()) != "Finish")
+            {
+
+                int places = int.Parse(Console.ReadLine());
+
+
+                for (int i = 0; i < places; i++)
+                {
+
+
+                    string typeTicket = Console.ReadLine();
+                    if (typeTicket == "End")
+                    {
+                        break;
+                    }
+
+                    else if (typeTicket == "standard")
+                    {
+                        percentStandard++;
+                    }
+                    else if (typeTicket == "student")
+                    {
+                        percentStudent++;
+                    }
+                    else if (typeTicket == "kid")
+                    {
+                        percentKid++;
+                    }
+                    totalTickets++;
+
+
+                    counter++;
+
+                }
+
+                percentFullHall = (double)counter / (double)places * 100.0;
+
+                Console.WriteLine("{0} - {1:F2}% full.", nameMovie, percentFullHall);
+
+
+                percentFullHall = 0;
+                counter = 0;
+
+            }
+
+
+            percentStudent = percentStudent / (double)totalTickets * 100.0;
+            percentStandard = percentStandard / (double)totalTickets * 100.0;
+            percentKid = percentKid / (double)totalTickets * 100.0;
+
+            Console.WriteLine("Total tickets: {0}\n{1:F2}% student tickets.\n{2:F2}% standard tickets.\n{3:F2}% kids tickets.", totalTickets, percentStudent, percentStandard, percentKid);
+
+        }
+    }
+}
---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7April2019/Cinema Voucher/Cinema Voucher/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Cinema_Voucher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vaucher = int.Parse(Console.ReadLine());
+            int counterTickets = 0;
+            int counterProducts = 0;
+
+            string text = null;
+
+            while ((text = Console.ReadLine()) != "End")
+            {
+
+
+                if (text.Length > 8)
+                {
+
+                    vaucher -= text[0];
+                    vaucher -= text[1];
+
+                    if (vaucher < 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        counterTickets++;
+                    }
+
+                }
+                else
+                {
+                    vaucher -= text[0];
+
+                    if (vaucher < 0)
+                    {
+
+                        break;
+
+
+                    }
+                    else
+                    {
+                        counterProducts++;
+                    }
+
+
+                }
+
+
+
+            }
+
+
+            Console.WriteLine("{0}\n{1}", counterTickets, counterProducts);
+        }
+    }
+}
