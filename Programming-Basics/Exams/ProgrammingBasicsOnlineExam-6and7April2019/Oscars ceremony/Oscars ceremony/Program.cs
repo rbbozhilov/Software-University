c6833bc8584eb2be56a4d39a27b45d9baa277a67
Rumen <rbbojilov@gmail.com>
2020-12-28 11:28:07 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7April2019/Oscars ceremony/Oscars ceremony/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Oscars_ceremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int naem = int.Parse(Console.ReadLine());
+
+            double priceStatuet = (double)naem - (naem * 0.3);
+            double priceketarink = priceStatuet - (priceStatuet * 0.15);
+            double priceSound = priceketarink / 2;
+
+            double totalPrice = naem + priceketarink + priceStatuet + priceSound;
+
+
+            Console.WriteLine("{0:F2}", totalPrice);
+        }
+    }
+}
