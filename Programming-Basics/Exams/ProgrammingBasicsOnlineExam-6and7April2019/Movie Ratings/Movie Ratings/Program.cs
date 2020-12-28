86a7bbae9e12a7e3b3267b6715a6012dbf1b9352
Rumen <rbbojilov@gmail.com>
2020-12-28 11:28:59 +0200

    Add

---
+++ Programming-Basics/Exams/ProgrammingBasicsOnlineExam-6and7April2019/Movie Ratings/Movie Ratings/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Movie_Ratings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int movies = int.Parse(Console.ReadLine());
+            double average = 0;
+            double maxReiting = double.MinValue;
+            double minReiting = double.MaxValue;
+            string hightReiting = null;
+            string lowerReiting = null;
+
+            for (int i = 0; i < movies; i++)
+            {
+
+                string name = Console.ReadLine();
+                double reiting = double.Parse(Console.ReadLine());
+
+
+
+                if (reiting > maxReiting)
+                {
+                    maxReiting = reiting;
+                    hightReiting = name;
+                }
+
+
+                if (reiting < minReiting)
+                {
+                    minReiting = reiting;
+                    lowerReiting = name;
+                }
+
+                average += reiting;
+
+            }
+
+            average = average / (double)movies;
+
+
+            Console.WriteLine("{0} is with highest rating: {1:F1}\n{2} is with lowest rating: {3:F1}\nAverage rating: {4:F1}", hightReiting, maxReiting, lowerReiting, minReiting, average);
+        }
+    }
+}
