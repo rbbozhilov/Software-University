75c3fdfc266c47a731c8ba527abd27cf58b34b36
Rumen <rbbojilov@gmail.com>
2020-12-29 10:22:13 +0200

    Add

---
+++ C#-Fundamentals/Arrays-Exercise/Top Integers/Top Integers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string[] stringArray = text.Split();
+
+
+            int[] integerArray = new int[stringArray.Length];
+
+
+
+
+
+            for (int i = 0; i < integerArray.Length; i++)
+            {
+                integerArray[i] = int.Parse(stringArray[i]);
+
+
+            }
+
+            int length = integerArray.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                bool flag = true;
+
+                for (int j = i + 1; j < length; j++)
+                {
+
+                    if (integerArray[i] <= integerArray[j])
+                    {
+                        flag = false;
+
+                    }
+
+                }
+
+                if (flag)
+                {
+                    Console.Write(integerArray[i] + " ");
+                }
+
+
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/Arrays-Exercise/Train/Train/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                array[i] = int.Parse(Console.ReadLine());
+                sum += array[i];
+
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                Console.Write(array[i] + " ");
+
+            }
+
+            Console.WriteLine("\n{0}", sum);
+        }
+    }
+}
