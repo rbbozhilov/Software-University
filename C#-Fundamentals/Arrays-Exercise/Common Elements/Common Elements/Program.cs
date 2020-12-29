cd5acba46c74ecdc2519f16cc890adaa56a6d1ff
Rumen <rbbojilov@gmail.com>
2020-12-29 10:24:35 +0200

    Add

---
+++ C#-Fundamentals/Arrays-Exercise/Common Elements/Common Elements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstText = Console.ReadLine();
+            string secondText = Console.ReadLine();
+
+            string[] firstArrray = firstText.Split();
+            string[] secondArray = secondText.Split();
+            int counter = 0;
+
+            string[] finalArray = new string[secondArray.Length];
+
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+
+                for (int j = 0; j < firstArrray.Length; j++)
+                {
+
+
+                    if (secondArray[i] == firstArrray[j])
+                    {
+
+                        finalArray[i] = secondArray[i];
+                        counter++;
+
+                    }
+                }
+
+            }
+
+
+            for (int i = 0; i < finalArray.Length; i++)
+            {
+                if (finalArray[i] == null)
+                {
+                    continue;
+                }
+                Console.Write(finalArray[i] + " ");
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/Arrays-Exercise/Equal Sum/Equal Sum/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int leftSum = 0;
+            int rightSum = 0;
+            string text = Console.ReadLine();
+            string[] arrayText = text.Split();
+
+            int[] integerArray = new int[arrayText.Length];
+
+            int length = integerArray.Length;
+            int currentIndex = 0;
+            bool flag = true;
+
+
+            for (int i = 0; i < length; i++)
+            {
+
+
+                integerArray[i] = int.Parse(arrayText[i]);
+
+
+            }
+
+
+            for (int i = 0; i < length; i++)
+            {
+
+
+                for (int k = i; k > 0; k--)
+                {
+
+
+                    leftSum += integerArray[i - k];
+
+
+                }
+
+                for (int j = i; j < length - 1; j++)
+                {
+
+                    rightSum += integerArray[j + 1];
+
+                }
+
+                if (rightSum == leftSum)
+                {
+                    currentIndex = i;
+                    flag = true;
+                    break;
+                }
+
+                else
+                {
+                    flag = false;
+                    leftSum = 0;
+                    rightSum = 0;
+                }
+
+            }
+
+
+            if (length > 1)
+            {
+                if (flag)
+                {
+                    Console.WriteLine(currentIndex);
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
