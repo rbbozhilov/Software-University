067e51cc1dbd42f2c5adb868f21eb1be6a5a64e1
Rumen <rbbojilov@gmail.com>
2020-12-29 10:23:38 +0200

    Add

---
+++ C#-Fundamentals/Arrays-Exercise/Magic Sum/Magic Sum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] arrayText = text.Split();
+
+            int length = arrayText.Length;
+
+            int[] array = new int[length];
+
+
+            for (int i = 0; i < length; i++)
+            {
+
+
+                array[i] = int.Parse(arrayText[i]);
+
+            }
+
+
+
+            int wantedSum = int.Parse(Console.ReadLine());
+
+            int currentSum = 0;
+
+
+            for (int i = 0; i < length; i++)
+            {
+
+
+                for (int j = i; j < length - 1; j++)
+                {
+
+
+                    currentSum = array[i] + array[j + 1];
+
+                    if (currentSum == wantedSum)
+                    {
+                        Console.Write(array[i] + " " + array[j + 1]);
+                        Console.WriteLine();
+                    }
+
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/Arrays-Exercise/Max Sequence of Equal Elements/Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+
+            string[] arrayText = text.Split();
+
+            int[] array = new int[arrayText.Length];
+
+            int length = array.Length;
+
+
+            for (int i = 0; i < length; i++)
+            {
+
+
+                array[i] = int.Parse(arrayText[i]);
+
+            }
+
+
+            int counterBest = 0;
+            int count = 0;
+            int bestNumber = 0;
+            int index = 0;
+
+
+            for (int i = 0; i < length; i++)
+            {
+
+
+                for (int j = i; j < length; j++)
+                {
+
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                        index = i;
+                    }
+                    else
+                    {
+
+                        break;
+                    }
+
+
+                }
+
+                if (count > counterBest)
+                {
+                    counterBest = count;
+                    bestNumber = array[index];
+                }
+
+                count = 0;
+            }
+
+
+            for (int i = 0; i < counterBest; i++)
+            {
+                Console.Write(bestNumber + " ");
+            }
+
+        }
+    }
+}
