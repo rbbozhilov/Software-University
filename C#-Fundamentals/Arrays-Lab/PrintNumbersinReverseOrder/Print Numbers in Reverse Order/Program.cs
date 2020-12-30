153a5174ede62ba3d8e1c040afea97091bb51120
Rumen <rbbojilov@gmail.com>
2020-12-30 12:27:51 +0200

    Add

---
+++ C#-Fundamentals/Arrays-Lab/PrintNumbersinReverseOrder/Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int[] array = new int[input];
+
+
+            int length = array.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+
+
+                array[i] = int.Parse(Console.ReadLine());
+
+
+            }
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+
+
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
