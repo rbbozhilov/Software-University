07191e32a9351dadae7d15db52118be81c646335
Rumen <rbbojilov@gmail.com>
2020-12-30 12:26:51 +0200

    Add

---
+++ C#-Fundamentals/Arrays-Lab/ReverseArrayofStrings/Reverse Array of Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string[] array = text.Split();
+
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+
+                var value = array[i];
+
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - i - 1] = value;
+
+            }
+
+
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/Arrays-Lab/RoundingNumbers/Rounding Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] array = Console.ReadLine().Split(' ').Select(decimal.Parse).ToArray();
+
+            foreach (double number in array)
+            {
+                int rounded = (int)Math.Round(number, MidpointRounding.AwayFromZero);
+                Console.WriteLine("{0} => {1}", number, rounded);
+            }
+        }
+    }
+}
