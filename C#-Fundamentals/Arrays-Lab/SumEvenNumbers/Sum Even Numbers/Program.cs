1aa757f980d79704a7b27bac20c644c2f7149427
Rumen <rbbojilov@gmail.com>
2020-12-30 12:25:39 +0200

    Add

---
+++ C#-Fundamentals/Arrays-Lab/SumEvenNumbers/Sum Even Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string[] array = text.Split();
+
+
+            int[] numbers = new int[array.Length];
+
+
+            int evenSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                numbers[i] = int.Parse(array[i]);
+
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+
+
+            }
+
+            Console.WriteLine(evenSum);
+        }
+    }
+}
