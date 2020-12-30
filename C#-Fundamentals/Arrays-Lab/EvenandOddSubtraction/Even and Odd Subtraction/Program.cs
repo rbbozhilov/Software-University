3189399d8fbbeeb9b5bbbee58ee1eebc4432d270
Rumen <rbbojilov@gmail.com>
2020-12-30 12:29:23 +0200

    Add

---
+++ C#-Fundamentals/Arrays-Lab/EvenandOddSubtraction/Even and Odd Subtraction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+
+            string[] stringArray = input.Split();
+
+            int[] integerArray = new int[stringArray.Length];
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < integerArray.Length; i++)
+            {
+
+                integerArray[i] = int.Parse(stringArray[i]);
+
+
+                if (integerArray[i] % 2 == 0)
+                {
+                    evenSum += integerArray[i];
+                }
+                else
+                {
+                    oddSum += integerArray[i];
+                }
+
+
+            }
+
+            Console.WriteLine(evenSum - oddSum);
+        }
+    }
+}
