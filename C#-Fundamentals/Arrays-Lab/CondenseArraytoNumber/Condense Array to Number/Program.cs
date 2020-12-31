0c128ac1e9e3ad2106a331c191c5dcb265ae076d
Rumen <rbbojilov@gmail.com>
2020-12-31 12:11:33 +0200

    Add

---
+++ C#-Fundamentals/Arrays-Lab/CondenseArraytoNumber/Condense Array to Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mainMasiv = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] condensedMasiv = new int[mainMasiv.Length - 1];
+
+            if (mainMasiv.Length == 1)
+            {
+                Console.WriteLine(mainMasiv[0]);
+                return;
+            }
+
+            for (int i = 0; i < mainMasiv.Length; i++)
+            {
+                for (int j = 0; j < condensedMasiv.Length - i; j++)
+                {
+                    condensedMasiv[j] = mainMasiv[j] + mainMasiv[j + 1];
+                }
+                mainMasiv = condensedMasiv;
+            }
+            Console.WriteLine(condensedMasiv[0]);
+        }
+    }
+}
---
+++ C#-Fundamentals/Arrays-Lab/DayofWeek/Day of Week/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+
+            string[] dayOfWeek = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+
+            if (input >= 1 && input <= 7)
+            {
+                Console.WriteLine(dayOfWeek[input - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
