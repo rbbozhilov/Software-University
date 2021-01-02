c89d6c761d1b523ca46cf81333b0cc254f589523
Rumen <rbbojilov@gmail.com>
2021-01-02 11:43:09 +0200

    Add

---
+++ C#-Fundamentals/AssociativeArrays-Lab/Word Synonyms/Word Synonyms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, List<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentWord = Console.ReadLine();
+                string currentSynonym = Console.ReadLine();
+
+                if (!dictionary.ContainsKey(currentWord))
+                {
+
+                    dictionary.Add(currentWord, new List<string>());
+
+                }
+
+                dictionary[currentWord].Add(currentSynonym);
+
+            }
+
+
+            foreach (var items in dictionary)
+            {
+
+                Console.Write("{0} - ", items.Key);
+
+                Console.WriteLine(string.Join(", ", items.Value));
+
+            }
+        }
+    }
+}
