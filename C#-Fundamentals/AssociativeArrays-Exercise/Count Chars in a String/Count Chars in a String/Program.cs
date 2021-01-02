eb17e49c6ff5551c5a8b2ff331f394ec266514cd
Rumen <rbbojilov@gmail.com>
2021-01-02 11:38:14 +0200

    Add

---
+++ C#-Fundamentals/AssociativeArrays-Exercise/Count Chars in a String/Count Chars in a String/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<char, int>();
+
+
+            string[] text = Console.ReadLine().Split();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                for (int j = 0; j < text[i].Length; j++)
+                {
+
+                    if (!dictionary.ContainsKey(text[i][j]))
+                    {
+
+                        dictionary.Add(text[i][j], 0);
+
+                    }
+
+                    dictionary[text[i][j]]++;
+
+                }
+
+
+            }
+
+
+            foreach (var items in dictionary)
+            {
+                Console.WriteLine("{0} -> {1}", items.Key, items.Value);
+            }
+
+        }
+    }
+}
---
+++ C#-Fundamentals/AssociativeArrays-Exercise/Courses/Courses/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var courses = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(" : ");
+
+                string currentCourse = tokens[0];
+                string currentStudent = tokens[1];
+                List<string> currentStudented = new List<string>();
+                currentStudented.Add(currentStudent);
+                if (!courses.ContainsKey(currentCourse))
+                {
+                    courses.Add(currentCourse, currentStudented);
+                }
+                else
+                {
+                    if (!courses.ContainsValue(currentStudented))
+                    {
+                        courses[currentCourse].Add(currentStudent);
+                    }
+
+                }
+
+            }
+
+            var sortedStudents = courses.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, k => k.Value);
+
+            foreach (var items in sortedStudents)
+            {
+
+                Console.WriteLine("{0}: {1}", items.Key, items.Value.Count);
+
+
+                var list = items.Value.OrderBy(x => x).ToList();
+
+                foreach (var itemss in list)
+                {
+                    Console.WriteLine("-- {0}", itemss);
+                }
+
+            }
+        }
+    }
+}
