b3165b4c74d3718f9a79eb46be8bea7a72516949
Rumen <rbbojilov@gmail.com>
2021-01-02 11:41:21 +0200

    Add

---
+++ C#-Fundamentals/AssociativeArrays-Exercise/AMinerTask/A Miner Task/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            string currentInput = string.Empty;
+            string input = string.Empty;
+            int counter = 1;
+
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+
+
+                if (counter % 2 == 1)
+                {
+                    currentInput = input;
+                    if (!dictionary.ContainsKey(input))
+                    {
+                        dictionary.Add(input, 0);
+                    }
+
+                }
+
+                else
+                {
+
+                    dictionary[currentInput] += int.Parse(input);
+
+
+                }
+
+                counter++;
+            }
+
+
+            foreach (var items in dictionary)
+            {
+                Console.WriteLine("{0} -> {1}", items.Key, items.Value);
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/AssociativeArrays-Exercise/CompanyUsers/Company Users/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var companies = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(" -> ");
+
+                string currentCompany = tokens[0];
+                string currentId = tokens[1];
+
+
+                if (!companies.ContainsKey(currentCompany))
+                {
+                    companies.Add(currentCompany, new List<string>());
+                    companies[currentCompany].Add(currentId);
+                }
+
+                else
+                {
+                    var list = new List<string>();
+                    list.Add(currentId);
+                    if (!companies[currentCompany].Contains(currentId))
+                    {
+                        companies[currentCompany].Add(currentId);
+                    }
+                }
+
+
+            }
+
+            var sortedCompanies = companies.OrderBy(x => x.Key);
+
+            foreach (var items in sortedCompanies)
+            {
+                Console.WriteLine(items.Key);
+
+                items.Value.ForEach(x => Console.WriteLine("-- {0}", x));
+
+            }
+        }
+    }
+}
