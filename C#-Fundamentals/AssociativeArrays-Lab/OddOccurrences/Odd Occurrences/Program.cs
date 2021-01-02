d3db873f9c8f2bae676d31d2bf5e07cc70327492
Rumen <rbbojilov@gmail.com>
2021-01-02 11:44:55 +0200

    Add

---
+++ C#-Fundamentals/AssociativeArrays-Lab/OddOccurrences/Odd Occurrences/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split();
+
+            var dic = new Dictionary<string, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                string currentWord = text[i].ToLower();
+
+                if (!dic.ContainsKey(currentWord))
+                {
+                    dic.Add(currentWord, 0);
+                }
+
+                dic[currentWord]++;
+
+
+            }
+
+            foreach (var items in dic)
+            {
+
+                if (items.Value % 2 == 1)
+                {
+                    Console.Write("{0} ", items.Key);
+                }
+
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/AssociativeArrays-Lab/WordFilter/Word Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            words = words.Where(x => x.Length % 2 == 0).ToArray();
+
+            foreach (var items in words)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
