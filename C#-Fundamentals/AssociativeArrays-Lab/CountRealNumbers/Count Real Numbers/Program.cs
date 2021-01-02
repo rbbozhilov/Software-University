9ffacd12f2f15fe8c44874bd6cf2b933daa51299
Rumen <rbbojilov@gmail.com>
2021-01-02 11:47:12 +0200

    Add

---
+++ C#-Fundamentals/AssociativeArrays-Lab/CountRealNumbers/Count Real Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (!dic.ContainsKey(numbers[i]))
+                {
+                    dic.Add(numbers[i], 0);
+                }
+
+                dic[numbers[i]]++;
+
+            }
+
+            dic = dic.OrderBy(x => x.Key).ToDictionary(x => x.Key, y => y.Value);
+
+            foreach (var items in dic)
+            {
+                Console.WriteLine("{0} -> {1}", items.Key, items.Value);
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/AssociativeArrays-Lab/Largest3Numbers/Largest 3 Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] largestNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            largestNumbers = largestNumbers.OrderByDescending(x => x).ToArray();
+            largestNumbers = largestNumbers.Take(3).ToArray();
+
+            Console.WriteLine(string.Join(" ", largestNumbers));
+        }
+    }
+}
