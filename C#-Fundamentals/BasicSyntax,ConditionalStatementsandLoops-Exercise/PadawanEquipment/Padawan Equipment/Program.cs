c36e1dcee6ab98ac09755a963c6545c89c02bc60
Rumen <rbbojilov@gmail.com>
2021-01-03 11:15:02 +0200

    Add

---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Exercise/PadawanEquipment/Padawan Equipment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceLightSabers = double.Parse(Console.ReadLine());
+            double priceRobes = double.Parse(Console.ReadLine());
+            double priceBelts = double.Parse(Console.ReadLine());
+
+            double totalSum = 0;
+
+            double procent = Math.Ceiling(students + (students * 0.1));
+
+            double allSabers = priceLightSabers * procent;
+
+            double allRobes = students * priceRobes;
+
+            int count = 0;
+
+            for (int i = 1; i <= students; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    count++;
+                }
+            }
+
+            double allBelts = priceBelts * (students - count);
+
+
+            totalSum = allBelts + allRobes + allSabers;
+
+            if (money >= totalSum)
+            {
+                Console.WriteLine("The money is enough - it would cost {0:F2}lv.", totalSum);
+            }
+            else
+            {
+                Console.WriteLine("Ivan Cho will need {0:F2}lv more.", Math.Abs(money - totalSum));
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Exercise/Printandsum/Print and sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Print_and_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+
+
+                if (i == end)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                    break;
+                }
+
+                Console.Write(i + " ");
+
+                sum += i;
+
+            }
+
+
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
