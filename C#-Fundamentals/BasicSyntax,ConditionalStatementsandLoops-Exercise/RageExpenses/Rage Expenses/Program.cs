9104795ad5cdb27198fd3692a063c20aeae8c064
Rumen <rbbojilov@gmail.com>
2021-01-03 11:13:36 +0200

    Add

---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Exercise/RageExpenses/Rage Expenses/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double priceHeadSet = double.Parse(Console.ReadLine());
+            double priceMouse = double.Parse(Console.ReadLine());
+            double priceKeyboard = double.Parse(Console.ReadLine());
+            double priceDisplay = double.Parse(Console.ReadLine());
+
+
+            int counter1 = 0;
+            int counter2 = 0;
+            int counter3 = 0;
+            int counter4 = 0;
+            int specialCounter = 0;
+            bool cer1 = false;
+            bool cer2 = false;
+
+            for (int i = 1; i <= lostGames; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    counter1++;
+                    cer1 = true;
+                }
+
+                if (i % 3 == 0)
+                {
+                    cer2 = true;
+                    counter2++;
+
+                }
+
+                if (cer1 == true && cer2 == true)
+                {
+
+
+                    counter3++;
+                    counter4++;
+
+                    if (counter4 % 2 == 0)
+                    {
+                        specialCounter++;
+                    }
+
+
+
+                }
+
+
+                cer1 = false;
+                cer2 = false;
+            }
+
+
+            double totalSum = (double)(counter1 * priceHeadSet) + (double)(counter2 * priceMouse) + (double)(counter3 * priceKeyboard) + (double)(specialCounter * priceDisplay);
+
+
+
+
+
+
+            Console.WriteLine("Rage expenses: {0:F2} lv.", totalSum);
+        }
+    }
+}
---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Exercise/Strongnumber/Strong number/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Strong_number
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+
+            int sum = 0;
+            for (int i = n; i >= 1; i--)
+            {
+
+                sum *= i;
+                if (n == i)
+                {
+                    sum = n;
+                }
+            }
+
+            if (n == 0)
+            {
+                sum = 1;
+            }
+
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            int finalSum = 0;
+            int value = 0;
+            int i = number;
+            int result = number;
+
+            while (i != 0)
+            {
+
+                result %= 10;
+
+                value = result;
+                i /= 10;
+                result = i;
+                finalSum += Factorial(value);
+
+            }
+
+            if (finalSum == number)
+            {
+                Console.WriteLine("yes");
+
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
