6d43891e3f523fa83369d800e8a12d515533452a
Rumen <rbbojilov@gmail.com>
2021-01-04 10:17:28 +0200

    Add

---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/SumofOddNumbers/Sum of Odd Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputIndex = int.Parse(Console.ReadLine());
+            int sumOdd = 0;
+            int counterOdd = 1;
+            for (int i = 0; i < inputIndex; i++)
+            {
+
+
+                Console.WriteLine(counterOdd);
+
+
+                sumOdd += counterOdd;
+                counterOdd += 2;
+            }
+
+            Console.WriteLine("Sum: " + sumOdd);
+        }
+    }
+}
---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/TheatrePromotion/Theatre Promotion/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayType = Console.ReadLine();
+
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+
+            if (age < 0 || age > 122)
+            {
+                Console.WriteLine("Error!");
+                System.Environment.Exit(1);
+            }
+
+
+
+            switch (dayType)
+            {
+
+
+                case "Weekday":
+                    {
+
+                        if (age >= 0 && age <= 18)
+                        {
+                            price = 12;
+                        }
+                        else if (age > 18 && age <= 64)
+                        {
+                            price = 18;
+                        }
+                        else if (age > 64 && age <= 122)
+                        {
+                            price = 12;
+                        }
+
+
+                        break;
+                    }
+
+                case "Weekend":
+                    {
+
+                        if (age > 0 && age <= 18)
+                        {
+                            price = 15;
+                        }
+                        else if (age > 18 && age <= 64)
+                        {
+                            price = 20;
+                        }
+                        else if (age > 64 && age <= 122)
+                        {
+                            price = 15;
+                        }
+
+                        break;
+
+                    }
+
+
+                case "Holiday":
+                    {
+
+                        if (age > 0 && age <= 18)
+                        {
+                            price = 5;
+                        }
+                        else if (age > 18 && age <= 64)
+                        {
+                            price = 12;
+                        }
+                        else if (age > 64 && age <= 122)
+                        {
+                            price = 10;
+                        }
+
+                        break;
+
+
+                    }
+
+
+            }
+
+            Console.WriteLine(price + "$");
+        }
+    }
+}
