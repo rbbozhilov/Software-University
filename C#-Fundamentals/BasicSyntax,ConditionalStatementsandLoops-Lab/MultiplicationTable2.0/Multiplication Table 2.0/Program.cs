50f1b4f3bfa6ec3aec24877c1eca48bd673a88ba
Rumen <rbbojilov@gmail.com>
2021-01-04 10:19:36 +0200

    Add

---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/MultiplicationTable2.0/Multiplication Table 2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputIndex = int.Parse(Console.ReadLine());
+            int inputOtherIndex = int.Parse(Console.ReadLine());
+            int counter = inputOtherIndex;
+
+            for (int i = inputOtherIndex; i <= 10; i++)
+            {
+
+
+                Console.WriteLine("{0} X {1} = {2}", inputIndex, counter, counter * inputIndex);
+                counter++;
+
+
+            }
+            if (inputOtherIndex > 10)
+            {
+                Console.WriteLine("{0} X {1} = {2}", inputIndex, counter, counter * inputIndex);
+            }
+        }
+    }
+}
