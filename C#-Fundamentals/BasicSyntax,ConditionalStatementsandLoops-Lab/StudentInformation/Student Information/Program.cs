497c1ce5fd122b60923e40714db920247a0e1119
Rumen <rbbojilov@gmail.com>
2021-01-04 10:18:39 +0200

    Add

---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/StudentInformation/Student Information/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Student_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: {0}, Age: {1}, Grade: {2:F2}", name, age, grade);
+        }
+    }
+}
