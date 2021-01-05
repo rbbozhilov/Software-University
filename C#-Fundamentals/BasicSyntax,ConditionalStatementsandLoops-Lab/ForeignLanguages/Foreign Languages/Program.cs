0e55e4522e1bdcf175c6e1f94e78b139157042fe
Rumen <rbbojilov@gmail.com>
2021-01-05 11:21:18 +0200

    Add

---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/ForeignLanguages/Foreign Languages/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+
+            switch (country)
+            {
+
+                case "England":
+                case "USA":
+                    {
+
+                        Console.WriteLine("English");
+                        break;
+                    }
+
+                case "Spain":
+                case "Mexico":
+                case "Argentina":
+                    {
+                        Console.WriteLine("Spanish");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("unknown");
+                        break;
+                    }
+
+
+
+            }
+        }
+    }
+}
