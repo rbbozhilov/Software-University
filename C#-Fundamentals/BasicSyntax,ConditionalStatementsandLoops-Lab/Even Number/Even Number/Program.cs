34da3f2fb4f6ec599d62be65af080fc7ef5e6909
Rumen <rbbojilov@gmail.com>
2021-01-05 11:22:57 +0200

    Add

---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/Even Number/Even Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            while (number % 2 != 0)
+            {
+
+                Console.WriteLine("Please write an even number.");
+
+                number = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("The number is: {0}", Math.Abs(number));
+        }
+    }
+}
