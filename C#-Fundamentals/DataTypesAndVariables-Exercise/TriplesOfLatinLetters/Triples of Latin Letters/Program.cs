a123f860d178919c611dec6bc05afa6f65e2dc68
Rumen <rbbojilov@gmail.com>
2021-01-05 11:26:59 +0200

    Add

---
+++ C#-Fundamentals/DataTypesAndVariables-Exercise/TriplesOfLatinLetters/Triples of Latin Letters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int j = 97; j < 97 + n; j++)
+            {
+
+                for (int k = 97; k < 97 + n; k++)
+                {
+
+                    for (int g = 97; g < 97 + n; g++)
+                    {
+
+                        Console.WriteLine("{0}{1}{2}", (char)j, (char)k, (char)g);
+
+                    }
+
+                }
+
+
+            }
+        }
+    }
+}
