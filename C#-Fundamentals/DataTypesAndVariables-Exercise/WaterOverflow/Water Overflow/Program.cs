2530d2ebbad6917f0e64d5e51e530b264da2d87e
Rumen <rbbojilov@gmail.com>
2021-01-05 11:25:32 +0200

    Add

---
+++ C#-Fundamentals/DataTypesAndVariables-Exercise/WaterOverflow/Water Overflow/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 255;
+
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int helper = 0;
+            for (int i = 0; i < number; i++)
+            {
+
+                int input = int.Parse(Console.ReadLine());
+
+                if (input > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+
+
+                sum += input;
+
+                if (sum > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sum -= input;
+                }
+
+
+            }
+
+
+            Console.WriteLine(sum);
+        }
+    }
+}
