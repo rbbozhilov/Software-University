9fc1d237e068224c2df6cc6ad89f160ef90703b9
Rumen <rbbojilov@gmail.com>
2021-01-05 11:24:14 +0200

    Add

---
+++ C#-Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/BackIn30Minutes/Back In 30 Minutes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                hours++;
+                minutes -= 60;
+            }
+            else
+            {
+
+            }
+
+            if (hours > 23)
+            {
+                hours -= 24;
+            }
+
+
+            Console.WriteLine("{0}:{1:d2}", hours, minutes);
+        }
+    }
+}
