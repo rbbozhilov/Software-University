2c5106f74e7e403e0ae43b8834a7e805ed4ab9b8
Rumen <rbbojilov@gmail.com>
2021-01-06 21:05:55 +0200

    Add

---
+++ C#-Fundamentals/DataTypesAndVariables-Exercise/BeerKegs/Beer Kegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            string best = string.Empty;
+            float bestSum = float.MinValue;
+
+            for (int i = 0; i < lines; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                float sum = ((float)Math.PI * radius * radius) * height;
+
+                if (sum > bestSum)
+                {
+                    bestSum = sum;
+                    best = model;
+                }
+
+            }
+
+            Console.WriteLine(best);
+        }
+    }
+}
