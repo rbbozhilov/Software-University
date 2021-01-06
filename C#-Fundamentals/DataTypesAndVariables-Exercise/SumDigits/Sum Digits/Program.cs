d400604291c771cbc32a5bd58dfbae40a95f4880
Rumen <rbbojilov@gmail.com>
2021-01-06 21:02:19 +0200

    Add

---
+++ C#-Fundamentals/DataTypesAndVariables-Exercise/SumDigits/Sum Digits/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digits = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            while (digits != 0)
+            {
+
+
+                sum += digits % 10;
+
+                digits /= 10;
+
+
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ C#-Fundamentals/DataTypesAndVariables-Exercise/SumofChars/Sum of Chars/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+
+            int sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+
+
+                char letter = char.Parse(Console.ReadLine());
+
+                sum += (int)letter;
+
+
+            }
+
+
+            Console.WriteLine("The sum equals: {0}", sum);
+        }
+    }
+}
