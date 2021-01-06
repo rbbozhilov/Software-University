b0e4f8795f4744a7ceb79c77597ea2fcb998757a
Rumen <rbbojilov@gmail.com>
2021-01-06 21:03:23 +0200

    Add

---
+++ C#-Fundamentals/DataTypesAndVariables-Exercise/Snowballs/Snowballs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberSnowBalls = int.Parse(Console.ReadLine());
+
+            BigInteger max = int.MinValue;
+            BigInteger sum = 0;
+            BigInteger lastSum = 0;
+            int saveSnowBallSnow = 0;
+            int saveSnowBallTime = 0;
+            int saveSnowBallUality = 0;
+            for (int i = 0; i < numberSnowBalls; i++)
+            {
+
+                int snowBallSnow = int.Parse(Console.ReadLine());
+                int snowBallTime = int.Parse(Console.ReadLine());
+                int snowBallUality = int.Parse(Console.ReadLine());
+
+
+                sum = (snowBallSnow / snowBallTime);
+                lastSum = BigInteger.Pow(sum, snowBallUality);
+
+
+
+                if (lastSum > max)
+                {
+                    max = lastSum;
+
+                    saveSnowBallSnow = snowBallSnow;
+                    saveSnowBallTime = snowBallTime;
+                    saveSnowBallUality = snowBallUality;
+
+                }
+
+            }
+
+
+            Console.WriteLine("{0} : {1} = {2} ({3})", saveSnowBallSnow, saveSnowBallTime, max, saveSnowBallUality);
+        }
+    }
+}
---
+++ C#-Fundamentals/DataTypesAndVariables-Exercise/SpiceMustFlow/Spice Must Flow/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int max = number;
+            int worker = number;
+            int total = 0;
+            int counter = 0;
+
+
+            if (number < 100)
+            {
+                Console.WriteLine("{0}\n{1}", counter, total);
+            }
+            else
+            {
+                while (true)
+                {
+
+
+                    if (max < 100)
+                    {
+
+                        total -= 26;
+                        break;
+
+
+                    }
+
+
+                    worker -= 26;
+                    total += worker;
+
+                    max -= 10;
+
+                    if (worker > max)
+                    {
+                        break;
+                    }
+
+                    counter++;
+                    worker = max;
+                }
+
+
+                Console.WriteLine("{0}\n{1}", counter, total);
+            }
+        }
+    }
+}
