e715ae0ed8dacdcdb1eed22ed48eee2b5a7a14e6
Rumen <rbbojilov@gmail.com>
2021-01-06 21:05:02 +0200

    Add

---
+++ C#-Fundamentals/DataTypesAndVariables-Exercise/Elevator/Elevator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            double courses = Math.Ceiling((double)people / (double)capacity);
+            int remainder = people % capacity;
+
+
+            Console.WriteLine(courses);
+        }
+    }
+}
