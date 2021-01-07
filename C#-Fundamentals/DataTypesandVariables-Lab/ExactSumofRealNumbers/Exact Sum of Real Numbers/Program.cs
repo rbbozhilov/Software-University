168002e68738c15d845e0d0e09d70899293701c2
Rumen <rbbojilov@gmail.com>
2021-01-07 11:33:19 +0200

    Add

---
+++ C#-Fundamentals/DataTypesandVariables-Lab/ExactSumofRealNumbers/Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal inputNumber = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                inputNumber += decimal.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine(inputNumber);
+        }
+    }
+}
---
+++ C#-Fundamentals/DataTypesandVariables-Lab/LowerorUpper/Lower or Upper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string character = Console.ReadLine();
+
+
+            int asciiCode = (int)character[0];
+
+
+
+            if (asciiCode >= 65 && asciiCode <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (asciiCode >= 97 && asciiCode <= 122)
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
