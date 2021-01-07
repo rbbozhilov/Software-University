1d4b51a082b072468979b461e34a0b965936dc70
Rumen <rbbojilov@gmail.com>
2021-01-07 11:32:00 +0200

    Add

---
+++ C#-Fundamentals/DataTypesandVariables-Lab/PoundstoDollars/Pounds to Dollars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double paunds = double.Parse(Console.ReadLine());
+
+
+            decimal result = (decimal)paunds * (decimal)1.31;
+
+            Console.WriteLine("{0:F3}", result);
+        }
+    }
+}
