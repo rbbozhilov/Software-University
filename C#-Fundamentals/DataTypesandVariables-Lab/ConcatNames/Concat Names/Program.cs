c70ba265b0c27998fc6602f31c913e1e2b7a7e76
Rumen <rbbojilov@gmail.com>
2021-01-07 11:34:27 +0200

    Add

---
+++ C#-Fundamentals/DataTypesandVariables-Lab/ConcatNames/Concat Names/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string other = Console.ReadLine();
+
+
+            Console.WriteLine("{0}{1}{2}", firstName, other, lastName);
+        }
+    }
+}
---
+++ C#-Fundamentals/DataTypesandVariables-Lab/ConvertMeterstoKilometers/Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+
+            decimal km = (decimal)meters / 1000;
+
+            Console.WriteLine("{0:F2}", km);
+        }
+    }
+}
