2c9b3dc133b5169d83b9ef5609588007502df410
Rumen <rbbojilov@gmail.com>
2021-01-07 11:31:02 +0200

    Add

---
+++ C#-Fundamentals/DataTypesandVariables-Lab/ReversedChars/Reversed Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char v = char.Parse(Console.ReadLine());
+
+
+            char a1 = v;
+            char b1 = b;
+            char c1 = a;
+
+
+            Console.WriteLine("{0} {1} {2}", a1, b1, c1);
+        }
+    }
+}
---
+++ C#-Fundamentals/DataTypesandVariables-Lab/SpecialNumbers/Special Numbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            bool checker = false;
+            int currentNumber = 0;
+            int otherCurrentNumber = 0;
+            for (int i = 1; i <= number; i++)
+            {
+
+
+                currentNumber = i;
+                otherCurrentNumber = i;
+
+                while (otherCurrentNumber != 0)
+                {
+
+
+
+
+                    otherCurrentNumber /= 10;
+
+                    sum += currentNumber % 10;
+                    currentNumber /= 10;
+
+                }
+
+
+                if (sum == 5)
+                {
+                    Console.WriteLine("{0} -> True", i);
+                }
+                else if (sum == 7)
+                {
+                    Console.WriteLine("{0} -> True", i);
+                }
+                else if (sum == 11)
+                {
+                    Console.WriteLine("{0} -> True", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} -> False", i);
+                }
+
+                sum = 0;
+            }
+        }
+    }
+}
