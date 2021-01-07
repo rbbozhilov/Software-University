8842e5e739caa83dac4ed4330c7838f27943e5ee
Rumen <rbbojilov@gmail.com>
2021-01-07 11:29:12 +0200

    Add

---
+++ C#-Fundamentals/DataTypesandVariables-Lab/TownInfo/Town Info/Town Info/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Town_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Town {0} has population of {1} and area {2} square km.", town, population, area);
+        }
+    }
+}
