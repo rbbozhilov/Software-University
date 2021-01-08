b3e4db8723b177a187f59cc4c2b46ba6e6484d05
Rumen <rbbojilov@gmail.com>
2021-01-08 16:14:37 +0200

    Add

---
+++ C#-Fundamentals/DataTypesandVariables-Lab/CharstoString/Chars to String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstLine = Console.ReadLine();
+            string secondLine = Console.ReadLine();
+            string thirdLine = Console.ReadLine();
+
+
+            Console.WriteLine(firstLine + secondLine + thirdLine);
+        }
+    }
+}
