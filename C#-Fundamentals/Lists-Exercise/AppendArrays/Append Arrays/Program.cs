8e9a640f34e1fbcb9be20f02f53b4f69779a586f
Rumen <rbbojilov@gmail.com>
2021-01-09 11:37:03 +0200

    Add

---
+++ C#-Fundamentals/Lists-Exercise/AppendArrays/Append Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('|').Reverse().ToArray();
+            string temporal = string.Join(" ", input);
+            string[] rezult = temporal.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Console.WriteLine(string.Join(" ", rezult));
+        }
+    }
+}
