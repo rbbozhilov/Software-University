25bd6c88c92d0059253a4231461eac0b42c75ed1
Rumen <rbbojilov@gmail.com>
2021-01-10 00:32:49 +0200

    Add

---
+++ C#-Fundamentals/Lists-Lab/ListOfProducts/List of Products/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            List<string> products = new List<string>(n);
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+                products.Add(Console.ReadLine());
+
+
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("{0}.{1}", i + 1, products[i]);
+
+            }
+        }
+    }
+}
