85df46d8781e8aacd29a34fcc6d65225c9b652fa
Rumen <rbbojilov@gmail.com>
2021-01-09 11:36:12 +0200

    Add

---
+++ C#-Fundamentals/Lists-Exercise/BombNumbers/Bomb Numbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+
+            int[] array = text.Split().Select(int.Parse).ToArray();
+
+            string[] other = Console.ReadLine().Split();
+
+
+            int power = int.Parse(other[0]);
+            int numbers = int.Parse(other[1]);
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+
+                if (array[i] == power)
+                {
+
+                    int start = Math.Max(0, i - numbers);
+                    int end = Math.Min(array.Length - 1, i + numbers);
+
+
+                    for (int j = i; j <= end; j++)
+                    {
+                        array[j] = 0;
+                    }
+
+
+                    for (int k = start; k < i; k++)
+                    {
+                        array[k] = 0;
+                    }
+
+
+                }
+
+
+            }
+
+
+            Console.WriteLine(array.Sum());
+
+        }
+    }
+}
