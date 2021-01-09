4584854cef22088e601494b40a4b4a06f20c0277
Rumen <rbbojilov@gmail.com>
2021-01-10 00:35:38 +0200

    Add

---
+++ C#-Fundamentals/Lists-Lab/Gauss'Trick/Gauss' Trick/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int count = numbers.Count;
+
+            for (int i = 0; i < count / 2; i++)
+            {
+
+
+                numbers[i] += numbers[count - i - 1];
+                numbers.RemoveAt(count - i - 1);
+
+
+            }
+
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
