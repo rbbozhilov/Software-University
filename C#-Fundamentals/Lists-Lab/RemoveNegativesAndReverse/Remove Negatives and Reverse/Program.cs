23e19e967b3107123868a0a8bc272e0ebb916f04
Rumen <rbbojilov@gmail.com>
2021-01-09 11:39:27 +0200

    Add

---
+++ C#-Fundamentals/Lists-Lab/RemoveNegativesAndReverse/Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+
+                    i--;
+
+                }
+
+
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                numbers.Reverse();
+
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+
+        }
+    }
+}
