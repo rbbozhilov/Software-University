c435163270cb574f41f887fe1759d87c1cb669e8
Rumen <rbbojilov@gmail.com>
2021-01-11 12:20:35 +0200

    Add

---
+++ C#-Fundamentals/Methods-Exercise/PalindromeIntegers/Palindrome Integers/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Palindrome_Integers
+{
+    class Program
+    {
+        static void GetArray(string text, int[] array)
+        {
+
+            int currentNumber = 0;
+            int result = int.Parse(text);
+            int count = 0;
+
+            while (result != 0)
+            {
+                currentNumber = result;
+
+                currentNumber %= 10;
+                array[count] = currentNumber;
+                result /= 10;
+                count++;
+            }
+
+        }
+
+
+        static bool Palindrome(int[] array)
+        {
+
+            bool flag = true;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i] != array[array.Length - i - 1])
+                {
+                    flag = false;
+                }
+
+
+            }
+
+            return flag;
+        }
+
+        static void Main(string[] args)
+        {
+
+            string text = string.Empty;
+
+            while ((text = Console.ReadLine()) != "END")
+            {
+
+                int[] array = new int[text.Length];
+                GetArray(text, array);
+                bool result = Palindrome(array);
+                if (result)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+
+        }
+    }
+}
