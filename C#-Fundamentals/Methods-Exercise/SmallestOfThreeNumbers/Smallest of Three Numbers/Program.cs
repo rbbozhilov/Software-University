62f866591ea1ef159732c1a7a85b7220d089a5e5
Rumen <rbbojilov@gmail.com>
2021-01-11 12:18:58 +0200

    Add

---
+++ C#-Fundamentals/Methods-Exercise/SmallestOfThreeNumbers/Smallest of Three Numbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static int SmallestNumber(int firstNum, int secondNum, int thirdNum)
+        {
+
+
+            int smallest = 0;
+
+            if (firstNum < secondNum)
+            {
+
+                smallest = firstNum;
+
+            }
+            else
+            {
+                smallest = secondNum;
+            }
+
+            if (smallest < thirdNum)
+            {
+                return smallest;
+            }
+            else
+            {
+                return thirdNum;
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(SmallestNumber(firstNumber, secondNumber, thirdNumber));
+
+
+        }
+    }
+}
