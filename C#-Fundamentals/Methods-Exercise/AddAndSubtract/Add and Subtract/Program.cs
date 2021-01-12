ee0fc9fe6d40f76a47894788b87fdf75a684fe28
Rumen <rbbojilov@gmail.com>
2021-01-12 12:22:34 +0200

    Add

---
+++ C#-Fundamentals/Methods-Exercise/AddAndSubtract/Add and Subtract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Add_and_Subtract
+{
+    class Program
+    {
+        static int SumDigits(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        static int Substract(int firstNumber, int secondNumber, int number)
+        {
+            return SumDigits(firstNumber, secondNumber) - number;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Substract(firstNumber, secondNumber, thirdNumber));
+
+
+
+        }
+    }
+}
