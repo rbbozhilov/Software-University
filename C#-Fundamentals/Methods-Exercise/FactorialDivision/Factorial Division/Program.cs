5948e8c591d18edf2f65003146a5808c5a5192c3
Rumen <rbbojilov@gmail.com>
2021-01-12 12:20:17 +0200

    Add

---
+++ C#-Fundamentals/Methods-Exercise/FactorialDivision/Factorial Division/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Factorial_Division
+{
+    class Program
+    {
+        static long Factorial(long number)
+        {
+
+            long result = 1;
+            if (number == 0 || number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = (int)number; i >= 1; i--)
+                {
+                    result *= i;
+                }
+                return result;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            long firstNumber = long.Parse(Console.ReadLine());
+            long secondNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("{0:F2}", (double)Factorial(firstNumber) / (double)Factorial(secondNumber));
+
+
+        }
+    }
+}
