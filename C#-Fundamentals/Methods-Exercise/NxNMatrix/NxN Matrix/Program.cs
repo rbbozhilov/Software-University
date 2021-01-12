d063b2abab02f57613411564749001fa1751ff59
Rumen <rbbojilov@gmail.com>
2021-01-12 12:18:34 +0200

    Add

---
+++ C#-Fundamentals/Methods-Exercise/NxNMatrix/NxN Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NxN_Matrix
+{
+    class Program
+    {
+        static void Matrix(int number)
+        {
+
+            for (int i = 0; i < number; i++)
+            {
+
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write(number + " ");
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            Matrix(number);
+
+        }
+    }
+}
