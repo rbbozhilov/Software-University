2844a99984ec7694a0436f9d582b3e914653e771
Rumen <rbbojilov@gmail.com>
2021-01-12 12:19:19 +0200

    Add

---
+++ C#-Fundamentals/Methods-Exercise/MiddleCharacters/Middle Characters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Middle_Characters
+{
+    class Program
+    {
+        static void CharsPrint(string text)
+        {
+
+
+            int length = text.Length;
+
+            if (length % 2 == 0)
+            {
+
+                Console.Write(text[length / 2 - 1]);
+                Console.WriteLine(text[length / 2]);
+
+            }
+            else
+            {
+                Console.WriteLine((char)text[length / 2]);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+
+            CharsPrint(text);
+
+
+        }
+    }
+}
