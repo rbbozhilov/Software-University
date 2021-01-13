8ad86749b5454311dd8e12a310908d0d79a232dd
Rumen <rbbojilov@gmail.com>
2021-01-14 01:03:29 +0200

    Add

---
+++ C#-Fundamentals/Methods-Lab/Calculations/Calculations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Calculator(string choice, int firstNumber, int secondNumber)
+        {
+
+            double result = 0;
+
+            switch (choice)
+            {
+                case "add":
+                    {
+                        result = (double)firstNumber + (double)secondNumber;
+                        break;
+                    }
+                case "divide":
+                    {
+                        result = (double)firstNumber / (double)secondNumber;
+                        break;
+                    }
+                case "subtract":
+                    {
+                        result = (double)firstNumber - (double)secondNumber;
+                        break;
+                    }
+                case "multiply":
+                    {
+                        result = (double)firstNumber * (double)secondNumber;
+                        break;
+                    }
+            }
+
+            Console.WriteLine(result);
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            string choice = Console.ReadLine();
+
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+
+            Calculator(choice, firstNum, secondNum);
+
+
+        }
+    }
+}
