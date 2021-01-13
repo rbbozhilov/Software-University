3981ba37294776ea825faca5e917002d477ea2eb
Rumen <rbbojilov@gmail.com>
2021-01-14 01:00:10 +0200

    Add

---
+++ C#-Fundamentals/Methods-Lab/MathPower/Math Power/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Math_Power
+{
+    class Program
+    {
+        static double MathPower(double number, int power)
+        {
+
+            double result = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+
+            return result;
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(MathPower(number, power));
+
+
+        }
+    }
+}
