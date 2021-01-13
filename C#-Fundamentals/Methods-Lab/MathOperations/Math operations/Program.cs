5fbc9560ac4d4affb65118094f9e43ab2fb351a5
Rumen <rbbojilov@gmail.com>
2021-01-14 01:01:00 +0200

    Add

---
+++ C#-Fundamentals/Methods-Lab/MathOperations/Math operations/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Math_operations
+{
+    class Program
+    {
+        static double MathOperations(string choice, double firstNumber, double secondNumber)
+        {
+
+            double result = 0;
+            switch (choice)
+            {
+                case "*":
+                    {
+
+                        result = firstNumber * secondNumber;
+
+                        break;
+                    }
+                case "/":
+                    {
+                        result = firstNumber / secondNumber;
+                        break;
+                    }
+                case "+":
+                    {
+                        result = firstNumber + secondNumber;
+                        break;
+                    }
+                case "-":
+                    {
+                        result = firstNumber - secondNumber;
+                        break;
+                    }
+
+
+            }
+
+
+            return result;
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            double firstNumber = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("{0}", MathOperations(operation, firstNumber, secondNumber));
+
+
+        }
+    }
+}
