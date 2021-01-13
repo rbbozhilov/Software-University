686e34ee3f581cde3696a43d16e7c43b0c614ab1
Rumen <rbbojilov@gmail.com>
2021-01-13 11:25:21 +0200

    Add

---
+++ C#-Fundamentals/Methods-Lab/Orders/Orders/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Orders
+{
+    class Program
+    {
+        static void PriceOfDrink(string drink, int number)
+        {
+            double price = 0;
+            switch (drink)
+            {
+
+                case "coffee":
+                    {
+                        price = 1.50;
+                        break;
+                    }
+                case "water":
+                    {
+                        price = 1;
+                        break;
+                    }
+                case "coke":
+                    {
+                        price = 1.40;
+                        break;
+                    }
+                case "snacks":
+                    {
+                        price = 2;
+                        break;
+                    }
+            }
+
+            price = price * number;
+
+            Console.WriteLine("{0:F2}", price);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            string choice = Console.ReadLine();
+
+            int number = int.Parse(Console.ReadLine());
+
+            PriceOfDrink(choice, number);
+
+
+        }
+    }
+}
