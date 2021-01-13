361007d731e120c3944ce1bca92933d7319f0586
Rumen <rbbojilov@gmail.com>
2021-01-14 01:02:20 +0200

    Add

---
+++ C#-Fundamentals/Methods-Lab/Grades/Grades/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void PrintGrade(double number)
+        {
+
+
+            string result = string.Empty;
+
+            if (number >= 2 && number <= 2.99)
+            {
+                result = "Fail";
+            }
+            else if (number >= 3 && number <= 3.49)
+            {
+                result = "Poor";
+            }
+            else if (number >= 3.50 && number <= 4.49)
+            {
+                result = "Good";
+            }
+            else if (number >= 4.50 && number <= 5.49)
+            {
+                result = "Very good";
+            }
+            else
+            {
+
+                result = "Excellent";
+
+            }
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            double number = double.Parse(Console.ReadLine());
+
+
+
+            PrintGrade(number);
+
+        }
+    }
+}
