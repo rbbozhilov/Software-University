6d959dee8deaf45ab61e073de8729b22a138f2c7
Rumen <rbbojilov@gmail.com>
2021-01-13 09:35:16 +0200

    Add

---
+++ C#-Fundamentals/Methods-Lab/RepeatString/Repeat String/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Repeat_String
+{
+    class Program
+    {
+        static string RepeatString(string text, int number)
+        {
+
+            string newString = string.Empty;
+
+            for (int i = 0; i < number; i++)
+            {
+
+                newString += text;
+
+            }
+
+
+            return newString;
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            string text = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(text, number));
+
+
+        }
+    }
+}
