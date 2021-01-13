da896ed17dc6692acc759c4144f87cba3ee5019d
Rumen <rbbojilov@gmail.com>
2021-01-13 09:34:24 +0200

    Add

---
+++ C#-Fundamentals/Methods-Lab/SignOfIntegerNumbers/Sign of Integer Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void PrintSign(int number)
+        {
+
+
+            string result = string.Empty;
+
+            if (number > 0)
+            {
+                result = "The number " + number + " is positive.";
+            }
+            else if (number == 0)
+            {
+                result = "The number " + number + " is zero.";
+            }
+            else
+            {
+                result = "The number " + number + " is negative.";
+            }
+
+
+            Console.WriteLine(result);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            int number = int.Parse(Console.ReadLine());
+
+
+
+            PrintSign(number);
+
+        }
+    }
+}
