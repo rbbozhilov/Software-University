d343ce69efb04caf56891cd13675a7ac6de87e28
Rumen <rbbojilov@gmail.com>
2021-01-15 19:05:57 +0200

    Add

---
+++ C#-Fundamentals/Methods-Lab/CalculateRectangleArea/Calculate Rectangle Area/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static int CalculateArea(int height, int weight)
+        {
+
+
+            return height * weight;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            int height = int.Parse(Console.ReadLine());
+            int weight = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(CalculateArea(height, weight));
+
+        }
+    }
+}
