3cbde012c3421b99fd77ff0abd78eb83142eab86
Rumen <rbbojilov@gmail.com>
2021-01-17 11:34:12 +0200

    Add

---
+++ C#-Fundamentals/ObjectsAndClasses-Lab/Randomize Words/Randomize Words/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string[] arrayText = text.Split();
+
+
+            Random rnd = new Random();
+
+            bool flag = false;
+            List<string> lists = new List<string>();
+            int lastIndex = arrayText.Length - 1;
+            int counter = 0;
+
+            do
+            {
+
+
+
+                int currentIndex = rnd.Next(0, lastIndex + 1);
+
+                string currentWord = arrayText[currentIndex];
+
+
+                if (counter == 0)
+                {
+                    lists.Add(currentWord);
+                    counter++;
+                    continue;
+                }
+
+                for (int i = 0; i < lists.Count; i++)
+                {
+
+
+                    if (currentWord == lists[i])
+                    {
+                        flag = true;
+
+                        break;
+                    }
+
+
+                }
+
+                if (!flag)
+                {
+                    lists.Add(currentWord);
+
+                    counter++;
+                }
+
+                flag = false;
+
+
+            }
+            while (counter < arrayText.Length);
+
+
+            foreach (string items in lists)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
