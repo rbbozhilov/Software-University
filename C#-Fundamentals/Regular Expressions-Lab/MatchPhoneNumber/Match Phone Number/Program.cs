056d10114c5a380e07d80eb5e0c055b76d26dfe3
Rumen <rbbojilov@gmail.com>
2021-01-19 00:36:31 +0200

    Add

---
+++ C#-Fundamentals/Regular Expressions-Lab/MatchPhoneNumber/Match Phone Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359([\s-])2\1(\d{3})\1(\d{4})\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(Console.ReadLine());
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
