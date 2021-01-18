5693621ce41349bef5c66ace0ed8b7201c32e6af
Rumen <rbbojilov@gmail.com>
2021-01-18 09:39:51 +0200

    Add

---
+++ C#-Fundamentals/ObjectsAndClasses-Lab/BigFactorial/Big Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger factorial = 1;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+
+
+                factorial *= i;
+
+
+
+            }
+
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
