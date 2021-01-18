9d2ab89c03dbee24111276cf67a5e4ef21404786
Rumen <rbbojilov@gmail.com>
2021-01-19 00:36:46 +0200

    Add

---
+++ C#-Fundamentals/Regular Expressions-Lab/MatchFullName/Match Full Name/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            var regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(Console.ReadLine());
+
+            foreach (var items in matches)
+            {
+                Console.Write(items + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
