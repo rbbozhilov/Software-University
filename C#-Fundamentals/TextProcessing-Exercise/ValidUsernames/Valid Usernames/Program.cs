e91d59ec2813c4ab4996f91bb2a17462fb086465
Rumen <rbbojilov@gmail.com>
2021-01-20 23:31:12 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Exercise/ValidUsernames/Valid Usernames/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] tokens = input.Split(", ");
+
+            bool flag = true;
+
+
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                flag = true;
+                if (tokens[i].Length >= 3 && tokens[i].Length <= 16)
+                {
+
+
+
+                    foreach (var items in tokens[i])
+                    {
+
+                        if (!char.IsDigit(items) && !char.IsLetter(items) && !(items == 95 || items == 45))
+                        {
+                            flag = false;
+
+                            break;
+                        }
+
+
+                    }
+
+                    if (flag)
+                    {
+                        Console.WriteLine(tokens[i]);
+
+                    }
+                }
+
+            }
+        }
+    }
+}
