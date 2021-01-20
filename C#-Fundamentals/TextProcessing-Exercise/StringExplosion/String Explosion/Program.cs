99fbcebb5150db4c8d85458e0d66621f6b5bd756
Rumen <rbbojilov@gmail.com>
2021-01-20 23:31:30 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Exercise/StringExplosion/String Explosion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int strength = 0;
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] == '>')
+                {
+
+                    strength += int.Parse(input[i + 1].ToString());
+                    continue;
+
+                }
+
+                if (strength > 0)
+                {
+
+                    input = input.Remove(i, 1);
+                    strength--;
+                    i--;
+                }
+
+
+
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
