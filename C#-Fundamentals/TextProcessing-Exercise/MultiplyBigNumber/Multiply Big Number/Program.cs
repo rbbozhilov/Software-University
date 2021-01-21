b96e656b82953bb45e2807abb2069865e385467e
Rumen <rbbojilov@gmail.com>
2021-01-21 18:44:33 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Exercise/MultiplyBigNumber/Multiply Big Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumString = Console.ReadLine();
+            bigNumString = bigNumString.TrimStart(new char[] { '0' });
+            char[] bigNum = bigNumString.ToCharArray();
+            int number = int.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            List<string> newNum = new List<string>();
+
+            int parse = 0;
+            for (int i = bigNum.Length - 1; i >= 0; i--)
+            {
+                parse = (int.Parse(Convert.ToString(bigNum[i])) * number) + parse;
+                newNum.Insert(0, (parse % 10).ToString());
+                parse /= 10;
+            }
+
+
+            if (parse > 0)
+                Console.WriteLine($"{parse}{string.Join("", newNum)}");
+            else
+                Console.WriteLine($"{string.Join("", newNum)}");
+        }
+    }
+}
