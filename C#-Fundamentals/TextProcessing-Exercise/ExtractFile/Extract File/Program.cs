449d218e50684bc3392e6bf2d3d7ad119d795cd9
Rumen <rbbojilov@gmail.com>
2021-01-21 18:45:13 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Exercise/ExtractFile/Extract File/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("\\");
+            string take = input[input.Length - 1];
+            string[] tokens = take.Split(".");
+            string name = tokens[0];
+            string ext = tokens[1];
+
+            Console.WriteLine("File name: {0}\nFile extension: {1}", name, ext);
+        }
+    }
+}
