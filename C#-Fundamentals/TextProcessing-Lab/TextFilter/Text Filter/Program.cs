2d82dea5d62d844548455e44310720f2fb28edf6
Rumen <rbbojilov@gmail.com>
2021-01-22 15:12:52 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Lab/TextFilter/Text Filter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] searchedWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < searchedWords.Length; i++)
+            {
+
+                if (text.Contains(searchedWords[i]))
+                {
+
+                    StringBuilder builder = new StringBuilder();
+
+                    for (int j = 0; j < searchedWords[i].Length; j++)
+                    {
+                        builder.Append("*");
+                    }
+                    text = text.Replace(searchedWords[i], builder.ToString());
+                }
+
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
