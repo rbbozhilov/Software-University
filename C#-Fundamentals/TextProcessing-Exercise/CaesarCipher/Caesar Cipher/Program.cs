b706e3f922f61529fb015a303e46bae7cda52b96
Rumen <rbbojilov@gmail.com>
2021-01-22 15:12:01 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Exercise/CaesarCipher/Caesar Cipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                result.Append((char)(text[i] + 3));
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
