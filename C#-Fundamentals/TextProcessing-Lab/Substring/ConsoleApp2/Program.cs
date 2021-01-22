ab05ff96686a4572614768853e9c757f8d1a2afc
Rumen <rbbojilov@gmail.com>
2021-01-22 15:13:03 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Lab/Substring/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedWord = Console.ReadLine();
+
+            string text = Console.ReadLine();
+
+            while (text.Contains(searchedWord, StringComparison.InvariantCultureIgnoreCase))
+            {
+
+                int currentIndex = text.IndexOf(searchedWord, StringComparison.InvariantCultureIgnoreCase);
+
+                text = text.Remove(currentIndex, searchedWord.Length);
+
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
