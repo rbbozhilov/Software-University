8a3923b5624a30d9e489c660cf97484b781dd0f0
Rumen <rbbojilov@gmail.com>
2021-01-22 15:13:19 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Lab/ReverseStrings/Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+
+                char[] reversedString = input.Reverse().ToArray();
+
+                Console.WriteLine("{0} = {1}", input, string.Join("", reversedString));
+
+            }
+        }
+    }
+}
