fc56ae2b509aa990c875b2634d6eb52a7fa00399
Rumen <rbbojilov@gmail.com>
2021-01-23 14:22:16 +0200

    Add

---
+++ C#-Fundamentals/TextProcessing-Lab/Digits,LettersAndOther/Digits, Letters and Other/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<char> characters = new List<char>();
+            List<char> numbers = new List<char>();
+            List<char> others = new List<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (char.IsDigit(input[i]))
+                {
+
+                    numbers.Add(input[i]);
+                }
+
+                else if (char.IsLetter(input[i]))
+                {
+                    characters.Add(input[i]);
+                }
+
+                else
+                {
+                    others.Add(input[i]);
+                }
+
+            }
+
+
+            Console.WriteLine("{0}\n{1}\n{2}", string.Join("", numbers), string.Join("", characters), string.Join("", others));
+        }
+    }
+}
---
+++ C#-Fundamentals/TextProcessing-Lab/RepeatStrings/Repeat Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string result = string.Empty;
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+
+                for (int j = 0; j < tokens[i].Length; j++)
+                {
+                    result += tokens[i];
+                }
+
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
