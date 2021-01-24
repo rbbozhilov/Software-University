3d7f681a014407db32e61dc7c5d696d27d7f71b2
Rumen <rbbojilov@gmail.com>
2021-01-24 20:18:49 +0200

    Add

---
+++ C#-Fundamentals/Exams/ProgrammingFundamentalsMidExamRetake-07April2020/CounterStrike/Counter Strike/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string distance = string.Empty;
+            int won = 0;
+
+            while ((distance = Console.ReadLine()) != "End of battle")
+            {
+
+
+                int tDistance = int.Parse(distance);
+
+                if (energy - tDistance >= 0)
+                {
+                    won++;
+                    energy -= tDistance;
+                    if (won % 3 == 0)
+                    {
+                        energy += won;
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Not enough energy! Game ends with {0} won battles and {1} energy", won, energy);
+                    Environment.Exit(0);
+                }
+
+
+            }
+
+
+            Console.WriteLine("Won battles: {0}. Energy left: {1}", won, energy);
+        }
+    }
+}
