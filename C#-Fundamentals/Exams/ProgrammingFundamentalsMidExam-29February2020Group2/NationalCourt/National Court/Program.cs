3605dfcbc88995ca4bbf6106ba193ee83206c8d2
Rumen <rbbojilov@gmail.com>
2021-01-25 21:18:01 +0200

    Add

---
+++ C#-Fundamentals/Exams/ProgrammingFundamentalsMidExam-29February2020Group2/NationalCourt/National Court/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmp = int.Parse(Console.ReadLine());
+            int secondEmp = int.Parse(Console.ReadLine());
+            int thirdEmp = int.Parse(Console.ReadLine());
+
+            int people = int.Parse(Console.ReadLine());
+
+
+            int answers = firstEmp + secondEmp + thirdEmp;
+            int count = 0;
+
+
+            while (true)
+            {
+
+
+                if (people <= 0)
+                {
+                    Console.WriteLine("Time needed: {0}h.", count);
+                    break;
+                }
+
+                count++;
+
+                if (count % 4 == 0 && count != 0)
+                {
+
+                    continue;
+                }
+
+                people -= answers;
+
+
+
+            }
+        }
+    }
+}
