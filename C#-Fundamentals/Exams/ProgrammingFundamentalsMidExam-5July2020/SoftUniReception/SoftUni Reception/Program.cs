6eb25fe8eff1afec4094df92122fb62b2c4f1d98
Rumen <rbbojilov@gmail.com>
2021-01-27 14:54:23 +0200

    Add

---
+++ C#-Fundamentals/Exams/ProgrammingFundamentalsMidExam-5July2020/SoftUniReception/SoftUni Reception/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SoftUni_Reception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmp = int.Parse(Console.ReadLine());
+            int secondEmp = int.Parse(Console.ReadLine());
+            int thirdEmp = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            int perHour = firstEmp + secondEmp + thirdEmp;
+
+
+            while (true)
+            {
+                if (students <= 0)
+                {
+                    break;
+                }
+
+                count++;
+
+                if (count % 4 == 0)
+                {
+                    continue;
+                }
+
+
+                students -= perHour;
+
+
+
+            }
+
+            Console.WriteLine("Time needed: {0}h.", count);
+        }
+    }
+}
