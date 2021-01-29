373a263bd96b4408da8c84fe0ce5d25ef7dc84a7
Rumen <rbbojilov@gmail.com>
2021-01-29 13:11:14 +0200

    Add

---
+++ C#-Fundamentals/Exams/ProgrammingFundamentalsMidExam-5July2020/Numbers/Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            numbers = numbers.Where(x => x > numbers.Average()).ToList();
+            numbers = numbers.OrderByDescending(x => x).ToList();
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                numbers = numbers.Take(5).ToList();
+
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
