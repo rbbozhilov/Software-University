bb4e2cd9f0692949359fc6e30e129ec665c6d906
Rumen <rbbojilov@gmail.com>
2021-02-11 12:44:38 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/SetsAndDictionariesAdvanced-Exercise/UniqueUsernames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for(int i=0;i<n;i++)
+            {
+
+                string currentName = Console.ReadLine();
+
+                names.Add(currentName);
+
+            }
+
+
+            foreach(var items in names)
+            {
+                Console.WriteLine(items);
+            }
+
+
+        }
+    }
+}
