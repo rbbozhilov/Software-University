09f0e7ad016c9f7a86e59be64794c85ec5584602
Rumen <rbbojilov@gmail.com>
2021-02-12 10:42:44 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/SetsAndDictionariesAdvanced-Exercise/PeriodicTable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> elements = new HashSet<string>();
+            for(int i=0;i<n;i++)
+            {
+
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach(var items in tokens)
+                {
+                    elements.Add(items);
+                }
+
+
+
+            }
+
+            elements = elements.OrderBy(x => x).ToHashSet();
+
+            Console.WriteLine(string.Join(" ",elements));
+
+
+        }
+    }
+}
