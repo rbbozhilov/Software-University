b8e5770f07890e44df507340b060c69c5994ff72
Rumen <rbbojilov@gmail.com>
2021-02-13 11:05:41 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/SetsAndDictionariesAdvanced-Exercise/EvenTimes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<n;i++)
+            {
+
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if(!numbers.ContainsKey(currentNumber))
+                {
+                    numbers[currentNumber] = 0;
+                }
+
+                numbers[currentNumber]++;
+
+            
+            }
+
+            int searchedEvenNumber = 0;
+
+            foreach(var items in numbers)
+            {
+
+                if(items.Value%2==0)
+                {
+                    searchedEvenNumber = items.Key;
+                    Console.WriteLine(searchedEvenNumber);
+                    break;
+                }
+
+
+            }
+
+
+        }
+    }
+}
