2e57ec0c5e26ed8cfcf1f148c6abc105386b52ce
Rumen <rbbojilov@gmail.com>
2021-02-13 11:05:20 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/SetsAndDictionariesAdvanced-Exercise/CountSymbols/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            var chars = new SortedDictionary<char, int>();
+
+            foreach(var items in text)
+            {
+
+                if(!chars.ContainsKey(items))
+                {
+                    chars.Add(items, 0);
+                }
+
+                chars[items]++;
+
+            }
+
+
+            foreach(var items in chars)
+            {
+
+                Console.WriteLine("{0}: {1} time/s",items.Key,items.Value);
+
+            }
+
+        }
+    }
+}
