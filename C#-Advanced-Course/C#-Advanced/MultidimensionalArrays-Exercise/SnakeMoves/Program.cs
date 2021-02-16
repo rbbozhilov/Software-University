98f2f8e02cf7f4ed1c3d04b801582cc1ae984c03
Rumen <rbbojilov@gmail.com>
2021-02-16 22:38:58 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/MultidimensionalArrays-Exercise/SnakeMoves/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] sizes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] snakeMoves = new int[sizes[0], sizes[1]];
+
+            string snake = Console.ReadLine();
+            StringBuilder snakePath = new StringBuilder();
+            int counter = 0;
+
+            for (int row = 0; row < snakeMoves.GetLength(0); row++)
+            {
+
+                if (row % 2 == 0)
+                {
+
+                    for (int col = 0; col < snakeMoves.GetLength(1); col++)
+                    {
+
+                        if(counter==snake.Length)
+                        {
+                            counter = 0;
+                        }
+
+                        snakeMoves[row, col] = snake[counter];
+
+                        counter++;
+
+                    }
+
+                }
+
+                else
+                {
+
+                    for(int col=snakeMoves.GetLength(1)-1;col>=0;col--)
+                    {
+
+                        if(counter==snake.Length)
+                        {
+                            counter = 0;
+                        }
+
+                        snakeMoves[row, col] = snake[counter];
+
+                        counter++;
+                    }
+
+                }
+
+
+            }
+
+            for(int row = 0;row<snakeMoves.GetLength(0);row++)
+            {
+
+               for(int col=0;col<snakeMoves.GetLength(1);col++)
+                {
+                    Console.Write((char)snakeMoves[row,col]);
+                }
+                Console.WriteLine();
+
+            }
+
+
+        }
+    }
+}
