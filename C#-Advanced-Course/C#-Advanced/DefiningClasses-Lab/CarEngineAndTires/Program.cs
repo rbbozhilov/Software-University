5815a91398aab629094907a934c097cab81898da
Rumen <rbbojilov@gmail.com>
2021-02-21 22:44:24 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Lab/CarEngineAndTires/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CarManufacturer
+{
+  public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
