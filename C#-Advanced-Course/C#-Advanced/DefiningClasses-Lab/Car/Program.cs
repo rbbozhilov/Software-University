2f169ec1a1cd00b9bacf897227339e92727411c6
Rumen <rbbojilov@gmail.com>
2021-02-23 12:44:14 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Lab/Car/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "NIK3";
+            car.Year = 1922;
+
+
+        }
+    }
+}
