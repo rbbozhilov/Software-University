75fe1ac8e02ad00d779a8208db5d41e0b4bce1ea
Rumen <rbbojilov@gmail.com>
2021-02-23 12:43:54 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Lab/Car/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+
+        //fields
+        private string make;
+        private string model;
+        private int year;
+
+        //Properties
+
+        public string Make 
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                this.year = value;
+            }
+        }
+
+
+
+    }
+}
