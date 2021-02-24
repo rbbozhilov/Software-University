0365eaf4f7b386fd43668ea66f9ae975daae9840
Rumen <rbbojilov@gmail.com>
2021-02-24 11:25:24 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Exercise/CarSalesman/Engine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesman
+{
+   public class Engine
+    {
+
+        public Engine()
+        {
+            this.Displacement = "n/a";
+            this.Efficiency = "n/a";
+        }
+
+        public Engine(string model,int power) :this()
+        {
+            this.Model = model;
+            this.Power = power;
+        }
+
+        public Engine(string model,int power,string displacement) :this(model,power)
+        {
+            this.Displacement = displacement;
+        }
+
+        public Engine(string model,string efficiency,int power) : this(model,power)
+        {
+            this.Efficiency = efficiency;
+        }
+
+        public Engine(string model,int power,string displacement, string efficiency) : this(model,power,displacement)
+        {
+            this.Efficiency = efficiency;
+        }
+
+        public string Model { get; set; }
+        public int Power { get; set; }
+        public string Displacement { get; set; }
+        public string Efficiency { get; set; }
+
+
+    }
+}
---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Exercise/CarSalesman/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarSalesman
+{
+  public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOfEngines = int.Parse(Console.ReadLine());
+            List<Engine> engines = new List<Engine>();
+
+            for(int i=0;i<numberOfEngines;i++)
+            {
+
+                string[] tokens = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                int length = tokens.Length;
+                string currentModel = tokens[0];
+                int currentPower = int.Parse(tokens[1]);
+
+                if(length==2)
+                {
+                    Engine currentEngine = new Engine(currentModel, currentPower);
+                    engines.Add(currentEngine);
+                }
+                else if(length==3)
+                {
+
+                    if(Char.IsDigit(tokens[2][0]))
+                    {
+                        Engine currentEngine = new Engine(currentModel, currentPower, tokens[2]);
+                        engines.Add(currentEngine);
+                    }
+                    else
+                    {
+                        Engine currentEngine = new Engine(currentModel, tokens[2], currentPower);
+                        engines.Add(currentEngine);
+                    }
+                    
+                }
+                else if(length==4)
+                {
+                    Engine currentEngine = new Engine(currentModel, currentPower,tokens[2], tokens[3]);
+                    engines.Add(currentEngine);
+                }
+
+
+            }
+
+            int numberOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for(int i=0;i<numberOfCars;i++)
+            {
+
+                string[] tokens = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                int length = tokens.Length;
+                string currentModel = tokens[0];
+                Engine currentEngine = engines.FirstOrDefault(x => x.Model == tokens[1]);
+
+                if(length==2)
+                {
+                    Car currentCar = new Car(currentModel, currentEngine);
+                    cars.Add(currentCar);
+                }
+                else if(length==3)
+                {
+
+                    if(Char.IsDigit(tokens[2][0]))
+                    {
+                        Car currentCar = new Car(currentModel, currentEngine, tokens[2]);
+                        cars.Add(currentCar);
+                    }
+                    else
+                    {
+                        Car currentCar = new Car(currentModel, tokens[2], currentEngine);
+                        cars.Add(currentCar);
+                    }
+
+
+                   
+                }
+                else if(length==4)
+                {
+                    Car curentCar = new Car(currentModel, currentEngine, tokens[2], tokens[3]);
+                    cars.Add(curentCar);
+                }
+
+
+            }
+
+
+            foreach(var items in cars)
+            {
+                Console.WriteLine(items.ToString());
+            }
+
+        }
+    }
+}
