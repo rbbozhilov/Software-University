8e759870b5fe16b5e45551d19ac7a2c2661fdf16
Rumen <rbbojilov@gmail.com>
2021-02-26 20:40:44 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Exercise/SoftUniParking/Car.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+
+        //Fields
+
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+
+        public Car(string make,string model,int horsePower,string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+
+        //Properties
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int HorsePower
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            set
+            {
+                this.horsePower = value;
+            }
+        }
+        public string RegistrationNumber
+        {
+            get
+            {
+                return this.registrationNumber;
+            }
+            set
+            {
+                this.registrationNumber = value;
+            }
+        }
+
+
+
+
+
+        public override string ToString()
+        {
+            string result = $"Make: {this.Make}{Environment.NewLine}";
+            result += $"Model: {this.Model}{Environment.NewLine}";
+            result += $"HorsePower: {this.HorsePower}{Environment.NewLine}";
+            result += $"RegistrationNumber: {this.RegistrationNumber}";
+
+            return result;
+        }
+
+
+    }
+}
---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Exercise/SoftUniParking/Parking.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+
+        //Fields
+
+        private int capacity;
+        private List<Car> cars;
+
+        //Properties
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            this.cars = new List<Car>(capacity);
+        }
+
+        //Properties
+
+        public int Count
+        {
+
+            get
+            {
+
+                return this.cars.Count;
+            }
+
+           
+
+        }
+
+        //Behavior
+
+        public Car GetCar(string registrationNumber)
+        {
+
+            Car currentCar = cars.FirstOrDefault(x => x.RegistrationNumber == registrationNumber);
+
+            return currentCar;
+        }
+
+        public string AddCar(Car currentCar)
+        {
+
+            Car searchedCar = cars.FirstOrDefault(x => x.RegistrationNumber == currentCar.RegistrationNumber);
+
+            if (searchedCar != null)
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (cars.Count >= capacity)
+            {
+                return "Parking is full!";
+            }
+
+            cars.Add(currentCar);
+
+            return $"Successfully added new car {currentCar.Make} {currentCar.RegistrationNumber}";
+        }
+
+
+        public string RemoveCar(string registrationNumber)
+        {
+
+            Car searchedCar = cars.FirstOrDefault(x => x.RegistrationNumber == registrationNumber);
+
+            if (searchedCar == null)
+            {
+
+                return "Car with that registration number, doesn't exist!";
+
+            }
+
+            cars.Remove(searchedCar);
+
+
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+
+            foreach (var items in RegistrationNumbers)
+            {
+
+                Car searchedCar = cars.FirstOrDefault(x => x.RegistrationNumber == items);
+
+                if (searchedCar != null)
+                {
+
+
+                    cars.Remove(searchedCar);
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
