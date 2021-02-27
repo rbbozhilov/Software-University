bd9a3dc3427b9a9ec61eb6ea32d184dd17085643
Rumen <rbbojilov@gmail.com>
2021-02-27 15:28:14 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Exercise/RawData/Tire.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Tire
+    {
+
+        public Tire(int age,double pressure)
+        {
+            this.Age = age;
+            this.Pressure = pressure;
+        }
+
+
+
+        public int Age { get; set; }
+        public double Pressure { get; set; }
+
+
+
+
+    }
+}
