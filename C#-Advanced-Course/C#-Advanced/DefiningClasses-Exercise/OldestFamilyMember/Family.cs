eb735ec51dc3a6d4eb45da0b9e17f2a2a27047db
Rumen <rbbojilov@gmail.com>
2021-03-01 00:24:14 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Exercise/OldestFamilyMember/Family.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DefiningClasses
+{
+   public class Family
+    {
+
+        //Fields
+        List<Person> people;
+
+        public Family()
+        {
+            people = new List<Person>();
+        }
+
+        public void Add(Person currentPerson)
+        {
+            people.Add(currentPerson);
+        }
+
+        public Person GetOldestMember()
+        {
+
+            Person oldestPerson = people[0];
+
+            foreach(var items in people)
+            {
+
+                if(oldestPerson.Age<items.Age)
+                {
+                    oldestPerson = items;
+                }
+
+
+            }
+
+           
+
+
+            return oldestPerson;
+
+        }
+
+
+    }
+}
