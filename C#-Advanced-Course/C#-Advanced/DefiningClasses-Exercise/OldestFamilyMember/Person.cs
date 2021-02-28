3d31fd99c00f5965d2b39c657fecbc48db8cf99e
Rumen <rbbojilov@gmail.com>
2021-03-01 00:25:05 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Exercise/OldestFamilyMember/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+
+        //Fields
+
+        private string name;
+        private int age;
+
+        //Constructors
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age) : this(age)
+        {
+            this.Name = name;
+        }
+
+        //Properties
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+
+
+    }
+}
