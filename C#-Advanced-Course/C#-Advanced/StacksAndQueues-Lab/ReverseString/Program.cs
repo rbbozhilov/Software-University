9e3ea76c87197623d55e88f5e4639928c4a828f0
Rumen <rbbojilov@gmail.com>
2021-03-03 01:30:50 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/StacksAndQueues-Lab/ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseString
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+            string text = Console.ReadLine();
+            Stack<char> basicReverse = new Stack<char>();
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                basicReverse.Push(text[i]);
+            }
+
+            while (basicReverse.Count != 0)
+            {
+                builder.Append(basicReverse.Pop());
+            }
+
+            Console.WriteLine(builder);
+
+
+        }
+    }
+}
