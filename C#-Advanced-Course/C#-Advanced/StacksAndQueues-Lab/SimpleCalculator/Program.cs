f51eb311a11dba5dc9be5db3768f3d72216ffde8
Rumen <rbbojilov@gmail.com>
2021-03-03 01:31:00 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/StacksAndQueues-Lab/SimpleCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] text = Console.ReadLine().Split();
+            text = text.Reverse().ToArray();
+            Stack<string> calculate = new Stack<string>(text);
+
+            while (calculate.Count != 1)
+            {
+                int currentNumber = int.Parse(calculate.Pop());
+                string operation = calculate.Pop();
+                int secondNumber = int.Parse(calculate.Pop());
+
+                if (operation == "+")
+                {
+                    calculate.Push((currentNumber + secondNumber).ToString());
+                }
+
+                else if (operation == "-")
+                {
+                    calculate.Push((currentNumber - secondNumber).ToString());
+                }
+
+
+
+            }
+
+            Console.WriteLine(calculate.Pop());
+
+        }
+    }
+}
