57a3765e4870121ed27a6b069559a7261ae7bcbe
Rumen <rbbojilov@gmail.com>
2021-03-02 23:05:08 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/DefiningClasses-Exercise/DefineAClassPerson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+        }
+    }
+}
