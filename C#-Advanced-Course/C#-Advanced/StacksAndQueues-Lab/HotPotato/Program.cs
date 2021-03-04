375bc20393dd1a87a14dbab5810186786c8353f3
Rumen <rbbojilov@gmail.com>
2021-03-04 21:51:12 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/StacksAndQueues-Lab/HotPotato/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> potatoPlayers = new Queue<string>(names);
+
+            int number = int.Parse(Console.ReadLine());
+            int counter = 0;
+            while (potatoPlayers.Count > 1)
+            {
+                counter++;
+
+                if (counter == number)
+                {
+                    Console.WriteLine("Removed {0}", potatoPlayers.Dequeue());
+                    counter = 0;
+                    continue;
+                }
+
+                potatoPlayers.Enqueue(potatoPlayers.Dequeue());
+
+
+            }
+
+            Console.WriteLine("Last is {0}", potatoPlayers.Dequeue());
+        }
+    }
+}
