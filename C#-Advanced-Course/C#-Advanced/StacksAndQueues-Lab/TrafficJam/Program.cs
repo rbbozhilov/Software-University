71b9eb5fc45c573d82db21c7ca2a5fd989890c2a
Rumen <rbbojilov@gmail.com>
2021-03-05 20:55:29 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/StacksAndQueues-Lab/TrafficJam/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = string.Empty;
+            Queue<string> customers = new Queue<string>();
+
+
+            while ((text = Console.ReadLine()) != "End")
+            {
+
+                if (text == "Paid" && customers.Count != 0)
+                {
+                    int count = customers.Count;
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+
+                }
+
+                else
+                {
+                    customers.Enqueue(text);
+                }
+
+
+            }
+
+            Console.WriteLine("{0} people remaining.", customers.Count);
+
+        }
+    }
+}
