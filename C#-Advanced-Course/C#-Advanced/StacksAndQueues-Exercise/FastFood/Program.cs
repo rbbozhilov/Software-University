3a83825fd0958537db6a2d2aab54471a9bd6842f
Rumen <rbbojilov@gmail.com>
2021-03-07 12:18:17 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/StacksAndQueues-Exercise/FastFood/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+            int maxOrders = int.Parse(Console.ReadLine());
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool flag = true;
+
+            Queue<int> allOrders = new Queue<int>(input);
+
+            Console.WriteLine(allOrders.Max());
+
+            while (allOrders.Count > 0)
+            {
+
+                int helper = maxOrders - allOrders.Peek();
+
+                if (helper < 0)
+                {
+                    Console.WriteLine("Orders left: " + string.Join(" ", allOrders));
+                    flag = false;
+                    break;
+                }
+
+                maxOrders -= allOrders.Dequeue();
+
+
+            }
+
+            if (flag)
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
