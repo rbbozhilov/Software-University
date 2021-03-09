5367d633d115f0e3180e73842ff69b0bbaf3f4d9
Rumen <rbbojilov@gmail.com>
2021-03-09 09:57:41 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/StacksAndQueues-Exercise/FashionBoutique/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var stack = new Stack<int>(input);
+            int count = 1;
+            int capacity = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            if (stack.Peek() == 0)
+            {
+                Console.WriteLine("0");
+                Environment.Exit(1);
+            }
+
+
+            while (stack.Count > 0)
+            {
+                int currentElement = stack.Pop();
+                sum += currentElement;
+
+                if (sum == capacity)
+                {
+                    count++;
+                    sum = 0;
+                }
+                else if (sum > capacity)
+                {
+                    stack.Push(currentElement);
+                    count++;
+                    sum = 0;
+                }
+
+
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
