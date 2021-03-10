0cab1911d79d078c2c28263f8b00fe79ec58b1db
Rumen <rbbojilov@gmail.com>
2021-03-10 23:54:28 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/StacksAndQueues-Exercise/TruckTour/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var petrols = new Queue<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                input += $" {i}";
+                petrols.Enqueue(input);
+
+
+            }
+
+            int totalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string currentInput = petrols.Dequeue();
+                string[] tokens = currentInput.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int currentPetrol = int.Parse(tokens[0]);
+                int currentDistance = int.Parse(tokens[1]);
+
+                totalSum += currentPetrol;
+
+                if (totalSum >= currentDistance)
+                {
+                    totalSum -= currentDistance;
+                }
+                else
+                {
+                    totalSum = 0;
+                    i = -1;
+                }
+
+                petrols.Enqueue(currentInput);
+            }
+
+
+            string[] result = petrols.Dequeue().Split();
+
+            Console.WriteLine(result[2]);
+        }
+    }
+}
