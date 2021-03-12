dfa0d1916a0fba4417cec0df76b97e33ddc68c9c
Rumen <rbbojilov@gmail.com>
2021-03-12 12:39:36 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/Functional Programming-Lab/SumNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] number = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Function(number, Count);
+            Function(number, Sum);
+
+        }
+
+        static void Function(int[] array, Func<int[], int> operation)
+        {
+
+            int result = operation(array);
+
+            Console.WriteLine(result);
+        }
+
+        static int Count(int[] array)
+        {
+            return array.Length;
+        }
+
+        static int Sum(int[] array)
+        {
+            return array.Sum();
+        }
+
+    }
+}
