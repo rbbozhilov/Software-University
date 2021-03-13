d7e15254e2d507446d8b65ec516c4ace72a664f0
Rumen <rbbojilov@gmail.com>
2021-03-13 23:49:43 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/Functional Programming-Lab/CountUppercaseWords/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Func<string, bool> checker = Check;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (checker(input[i]))
+                {
+                    Console.WriteLine(input[i]);
+                }
+
+
+            }
+
+        }
+
+
+        static bool Check(string currentWord)
+        {
+
+            if (currentWord[0] == currentWord.ToUpper()[0])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+
+}
+
+
