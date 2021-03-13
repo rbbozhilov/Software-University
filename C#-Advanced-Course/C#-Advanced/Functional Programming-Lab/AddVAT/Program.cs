ed7f75e557fbe002ecd76fe7e183a1d8277fd91a
Rumen <rbbojilov@gmail.com>
2021-03-13 23:49:14 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/Functional Programming-Lab/AddVAT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] prices = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).Select(s => s * 1.2m).ToArray();
+
+
+            prices.ToList().ForEach(s => Console.WriteLine("{0:F2}", s));
+        }
+    }
+}
