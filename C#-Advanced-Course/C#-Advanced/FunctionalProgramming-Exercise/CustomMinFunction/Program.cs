26570ee3e8350ccfa704b1a7991cbb68044807f9
Rumen <rbbojilov@gmail.com>
2021-03-16 00:04:57 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/FunctionalProgramming-Exercise/CustomMinFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = x => int.Parse(x);
+
+            int[] numbers = Console.ReadLine().Split().Select(parser).ToArray();
+
+            Func<int[], int> minValue = x =>
+            {
+
+                int min = int.MaxValue;
+
+                foreach (var items in numbers)
+                {
+                    if (items < min)
+                    {
+                        min = items;
+                    }
+                }
+                return min;
+            };
+
+
+            Console.WriteLine(minValue(numbers));
+        }
+    }
+}
