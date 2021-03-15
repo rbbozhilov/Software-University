61f937b7b9953d0e8253e03d4240fdbcfd742dae
Rumen <rbbojilov@gmail.com>
2021-03-15 23:54:06 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/FunctionalProgramming-Exercise/ActionPrint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var items in names)
+            {
+                Action<string> printer = Print(items);
+                printer(items);
+            }
+
+        }
+
+        static Action<string> Print(string currentName)
+        {
+
+            return currentName => Console.WriteLine(currentName);
+
+        }
+    }
+}
