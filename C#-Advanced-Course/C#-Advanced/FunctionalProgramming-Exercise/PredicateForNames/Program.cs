0cb1a3e3c682b5b0a5fff7aa118d6ce595128a47
Rumen <rbbojilov@gmail.com>
2021-03-17 23:49:28 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/FunctionalProgramming-Exercise/PredicateForNames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> func = x =>
+            {
+
+                if (x.Length <= length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            };
+
+            names = names.Where(func).ToArray();
+
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
