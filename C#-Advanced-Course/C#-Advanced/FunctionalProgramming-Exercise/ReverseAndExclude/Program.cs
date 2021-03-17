7c80acbd7480b3d3e7e2e09dd6dbc8e476a10d9b
Rumen <rbbojilov@gmail.com>
2021-03-18 00:13:28 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/FunctionalProgramming-Exercise/ReverseAndExclude/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Reverse().ToList();
+
+            int number = int.Parse(Console.ReadLine());
+
+
+            Func<int, bool> func = x => x % number != 0;
+
+            numbers = numbers.Where(func).ToList();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
