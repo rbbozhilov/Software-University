01f54bb73a3d86a7bb13656c411cf878c4ab58a5
Rumen <rbbojilov@gmail.com>
2021-03-20 23:25:59 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/IteratorsAndComparators-Lab/Library/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+
+        public Book(string title, int year, params string[] names)
+        {
+
+            this.Title = title;
+            this.Year = year;
+            this.Authors = names.ToList();
+
+        }
+
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+
+
+            if (this.Year.CompareTo(other.Year) != 0)
+            {
+                return this.Year.CompareTo(other.Year);
+            }
+
+            return this.Title.CompareTo(other.Title);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+
+    }
+}
