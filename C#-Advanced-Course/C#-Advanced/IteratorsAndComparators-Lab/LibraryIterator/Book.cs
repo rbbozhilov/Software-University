ac9f8165c2a8ba2486bca38439ed1c7e0493a79f
Rumen <rbbojilov@gmail.com>
2021-03-20 23:26:24 +0200

    Add

---
+++ C#-Advanced-Course/C#-Advanced/IteratorsAndComparators-Lab/LibraryIterator/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+
+        public Book(string title,int year,params string[] names)
+        {
+
+            this.Title = title;
+            this.Year = year;
+            this.Authors = names.ToList();
+
+        }
+
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+
+
+    }
+}
