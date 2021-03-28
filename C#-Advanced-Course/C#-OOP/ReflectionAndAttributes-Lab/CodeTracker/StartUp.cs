66b6556f26549d4c238975adf38c3ec9b6dbff82
Rumen <rbbojilov@gmail.com>
2021-03-28 22:40:41 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/ReflectionAndAttributes-Lab/CodeTracker/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthorProblem
+{
+    [Author("Gosho")]
+    public class StartUp
+    {
+        [Author("Niki")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
