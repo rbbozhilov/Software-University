f140677270bec7a0f113ce0ed8d8445aa30ca374
Rumen <rbbojilov@gmail.com>
2021-03-28 22:40:49 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/ReflectionAndAttributes-Lab/Collector/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stealer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Spy spy = new Spy();
+
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result);
+
+        }
+    }
+}
