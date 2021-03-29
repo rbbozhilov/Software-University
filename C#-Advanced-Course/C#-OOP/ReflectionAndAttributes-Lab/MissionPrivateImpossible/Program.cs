e725b3ba01ebcc160ec0427f5aa3cee7e2a186d5
Rumen <rbbojilov@gmail.com>
2021-03-29 23:51:06 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/ReflectionAndAttributes-Lab/MissionPrivateImpossible/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stealer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Spy spy = new Spy();
+
+            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(result);
+
+        }
+    }
+}
