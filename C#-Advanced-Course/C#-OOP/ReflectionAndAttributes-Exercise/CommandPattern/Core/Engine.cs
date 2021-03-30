29d9cda06271a12b5705bba576adb66d1f4301b1
Rumen <rbbojilov@gmail.com>
2021-03-30 23:49:04 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/ReflectionAndAttributes-Exercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,45 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+
+        public void Run()
+        {
+            
+            while(true)
+            {
+
+                string arguments = Console.ReadLine();
+
+                try
+                {
+
+                    string result = this.commandInterpreter.Read(arguments);
+
+
+                }
+
+                catch(ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+
+            }
+
+
+        }
+    }
+}
