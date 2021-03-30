d8cc21055890e7af2104b106c5c2e3affadd18be
Rumen <rbbojilov@gmail.com>
2021-03-30 23:48:52 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/ReflectionAndAttributes-Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,43 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+
+        private const string COMMAND_COM = "Command";
+
+        public CommandInterpreter()
+        {
+
+        }
+
+
+        public string Read(string args)
+        {
+
+            string[] commandTokens = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string command = commandTokens[0]+COMMAND_COM;
+            string[] commandArgs = commandTokens.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type commandType = assembly.GetTypes().FirstOrDefault(x => x.Name.ToLower() == command.ToLower());
+
+            if(commandType==null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            ICommand commandIstance = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = commandIstance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
