884240c358f3edbae8ddfc3b106dfce7315ba767
Rumen <rbbojilov@gmail.com>
2021-03-31 23:26:18 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/ReflectionAndAttributes-Exercise/ValidationAttributes/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Models
+{
+    public class Person
+    {
+
+        public Person(string fullName,int age)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+        }
+
+
+        [MyRequiredAttribute]
+        public string FullName { get; set; }
+
+        [MyRange(12,90)]
+        public int Age { get; set; }
+
+
+
+    }
+}
