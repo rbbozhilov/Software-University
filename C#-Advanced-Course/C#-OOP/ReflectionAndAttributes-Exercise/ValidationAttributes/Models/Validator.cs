f7e0902dd5d3ddead05f37c07785587c61cadcde
Rumen <rbbojilov@gmail.com>
2021-03-31 23:26:28 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/ReflectionAndAttributes-Exercise/ValidationAttributes/Models/Validator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidationAttributes.Attributes;
+using System.Linq;
+using System.Reflection;
+
+namespace ValidationAttributes.Models
+{
+   public static class Validator 
+    {
+
+        public static bool IsValid(object obj)
+        {
+
+            if(obj==null)
+            {
+                return false;
+            }
+
+            Type type = obj.GetType();
+            var properties = type.GetProperties();
+
+            foreach(var propertie in properties)
+            {
+
+                MyValidationAttribute[] attributes = propertie
+                    .GetCustomAttributes()
+                    .Where(ca => ca is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+
+                foreach(var attribute in attributes)
+                {
+                    if(!attribute.IsValid(propertie.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+
+            }
+
+
+            return true;
+        }
+
+
+    }
+}
