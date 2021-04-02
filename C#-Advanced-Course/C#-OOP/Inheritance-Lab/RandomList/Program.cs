58f3794b65f2869f47a0e79d496d22b00994797e
Rumen <rbbojilov@gmail.com>
2021-04-03 02:35:23 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Inheritance-Lab/RandomList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            RandomList list = new RandomList();
+
+
+            list.Add("13");
+            list.Add("312314dsd");
+            list.Add("dddddddddddd");
+            list.Add("eeeeeeeeeeeeeee");
+            list.Add("vvvvvvvvvvvv");
+
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+
+        }
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/Inheritance-Lab/RandomList/RandomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+   public  class RandomList : List<string>
+    {
+
+        public string RandomString()
+        {
+
+            Random number = new Random();
+            int index = number.Next(0, this.Count);
+            string currentValue = this[index];
+            this.Remove(this[index]);
+            return currentValue;
+        }
+
+
+    }
+}
