b04429b5011b26329358c5e32f2715b90416db62
Rumen <rbbojilov@gmail.com>
2021-04-03 02:35:52 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Inheritance-Lab/StackOfStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+
+        }
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/Inheritance-Lab/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+   public class StackOfStrings : Stack<string>
+    {
+
+
+        public bool IsEmpty()
+        {
+
+            if(this.Count>0)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public void AddRange(Stack<string> stack)
+        {
+
+            while(stack.Count>0)
+            {
+                this.Push(stack.Pop());
+            }
+
+        }
+
+
+    }
+}
