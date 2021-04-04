e8baa0c194ec271415f0ee45e772278b6a63000d
Rumen <rbbojilov@gmail.com>
2021-04-04 18:01:19 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Inheritance-Lab/Inheritance-Exercise/Person/Child.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+    }
+}
