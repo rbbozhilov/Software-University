89454248d2668ab509b601c89aa4f381910d418d
Rumen <rbbojilov@gmail.com>
2021-04-05 23:27:42 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Inheritance-Exercise/Person/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class Person
+    {
+
+        private string name;
+        private int age;
+
+        public Person(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name { get { return this.name; } set { this.name = value; } }
+        public int Age
+        {
+            get
+            {
+
+                return this.age;
+
+            }
+            set
+            {
+
+             
+                    if(value<0)
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    else
+                    {
+                        this.age = value;
+                    }
+                
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+
+    }
+}
