d325fe6c002cc5d6199f001a8d08c6c252b7d898
Rumen <rbbojilov@gmail.com>
2021-04-07 23:01:25 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Inheritance-Exercise/NeedForSpeed/FamilyCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+
+
+        
+
+
+    }
+}
