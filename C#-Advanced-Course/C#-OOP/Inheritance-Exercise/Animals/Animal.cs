0b47e268936504502a411795664ee19e7dbb2312
Rumen <rbbojilov@gmail.com>
2021-04-11 20:11:38 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Inheritance-Exercise/Animals/Animal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))    
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Gender { get { return this.gender; } private set { if (value != "Male" && value != "Female") { throw new ArgumentException("Invalid input!"); } this.gender = value; } }
+
+
+        public abstract string ProduceSound();
+
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(this.GetType().Name)
+                .AppendLine($"{this.Name} {this.Age} {this.Gender.ToString()}")
+                .Append($"{this.ProduceSound()}");
+
+            return builder.ToString();
+        }
+
+    }
+}
