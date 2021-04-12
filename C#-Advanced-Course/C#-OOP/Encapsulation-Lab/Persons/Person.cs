4f63fbeddbe4468930e8047a006807984b6c5fe1
Rumen <rbbojilov@gmail.com>
2021-04-12 22:06:07 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Encapsulation-Lab/Persons/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+
+        private int age;
+        private string firstName;
+        private string lastName;
+
+        public Person(string firstName,string lastName,int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+
+
+        public string FirstName { get { return this.firstName; } private set { this.firstName = value; } }
+        public string LastName { get { return this.lastName; } private set { this.lastName = value; } }
+        public int Age { get { return this.age; } set { this.age = value; } }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+
+    }
+}
