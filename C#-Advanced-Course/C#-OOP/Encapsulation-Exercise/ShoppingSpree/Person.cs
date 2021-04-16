0ca9d4f4c01fd85f9ecc33302e6c2f1a999d831e
Rumen <rbbojilov@gmail.com>
2021-04-16 11:23:25 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Encapsulation-Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+
+        private string name;
+        private double money;
+        private List<Product> products;
+
+        public Person(string name, double money)
+        {
+            this.Name = name;
+            this.Money = money;
+            products = new List<Product>();
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new System.ArgumentException("Name cannot be empty");
+                    
+                }
+                this.name = value;
+            }
+        }
+        public double Money
+        {
+            get
+            {
+                return this.money;
+
+            }
+           private  set
+            {
+
+                if(value<0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                    
+                }
+
+                this.money = value;
+            }
+        }
+        public IReadOnlyCollection<Product> BagOfProducts 
+        {
+            get
+            {
+                return this.products.AsReadOnly();
+            }
+            
+        }
+
+        public void Adder(Product currentProduct)
+        {
+
+            if(currentProduct.Cost<=this.money)
+            {
+                this.money -= currentProduct.Cost;
+                Console.WriteLine("{0} bought {1}",this.name,currentProduct.Name);
+                products.Add(currentProduct);
+            }
+            else
+            {
+
+                Console.WriteLine("{0} can't afford {1}",this.name,currentProduct.Name);
+
+            }
+           
+            
+        }
+
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/Encapsulation-Exercise/ShoppingSpree/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+
+        private string name;
+        private double cost;
+
+        public Product(string name, double cost)
+        {
+
+            this.Name = name;
+            this.Cost = cost;
+
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                   
+                }
+
+                this.name = value;
+
+            }
+        }
+
+
+        public double Cost
+        {
+            get
+            {
+                return this.cost;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                    
+                    
+                }
+
+                this.cost = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
