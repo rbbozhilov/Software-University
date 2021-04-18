edd92e151e79b2536ea35d4d6ee11c47a5b840ab
Rumen <rbbojilov@gmail.com>
2021-04-19 00:00:59 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Lab/Shapes/Program.cs
@@ -0,0 +1,25 @@
+﻿using AbstractionsAndInterfacesExcr.Contracts;
+using Shapes.Models;
+using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+
+            circle.Draw();
+            rect.Draw();
+
+
+        }
+    }
+}
