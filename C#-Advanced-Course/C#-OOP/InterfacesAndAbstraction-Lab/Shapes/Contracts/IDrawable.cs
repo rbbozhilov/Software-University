ff22f1a8eb552d11d25eaa7208f8364d51ddff9f
Rumen <rbbojilov@gmail.com>
2021-04-19 00:00:29 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Lab/Shapes/Contracts/IDrawable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractionsAndInterfacesExcr.Contracts
+{
+   public interface IDrawable
+    {
+
+
+        void Draw();
+
+    }
+}
