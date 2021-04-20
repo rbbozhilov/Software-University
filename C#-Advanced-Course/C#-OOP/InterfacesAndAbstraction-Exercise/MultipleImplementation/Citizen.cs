4279931685def38a125b8ff46e22d1fefb4d1c3b
Rumen <rbbojilov@gmail.com>
2021-04-21 00:26:51 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/MultipleImplementation/Citizen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson , IIdentifiable , IBirthable
+    {
+
+        private string name;
+        private int age;
+        private string birthDate;
+        private string id;
+
+        public Citizen(string name,int age , string id , string birthDate)
+        {
+            this.name = name;
+            this.age = age;
+            this.birthDate = birthDate;
+            this.id = id;
+        }
+
+        public string Name => this.name;
+
+        public int Age => this.age;
+
+        public string Id => this.id;
+
+        public string Birthdate => this.birthDate;
+
+
+
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/MultipleImplementation/Contracts/IBirthable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface IBirthable
+    {
+
+        public string Birthdate { get;  }
+
+    }
+}
