7be3d9ed62dbc5531f003113df9ae72a6578c584
Rumen <rbbojilov@gmail.com>
2021-04-22 10:22:57 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/Telephony/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Telephony.Contracts;
+
+namespace Telephony
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] phoneNumbers = Console.ReadLine().Split(" ");
+
+            for(int i=0;i<phoneNumbers.Length;i++)
+            {
+
+                if(phoneNumbers[i].Length==7)
+                {
+                    ICall caller = new StationaryPhone();
+                    caller.Call(phoneNumbers[i]);
+                }
+
+                else
+                {
+                    ICall caller = new Smartphone();
+                    caller.Call(phoneNumbers[i]);
+                }
+
+            }
+
+            string[] browsers = Console.ReadLine().Split(" ");
+
+            for(int i=0;i<browsers.Length;i++)
+            {
+
+                IBrowse browse = new Smartphone();
+                browse.Browse(browsers[i]);
+
+            }
+
+
+        }
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/Telephony/Smartphone.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telephony.Contracts;
+using System.Linq;
+
+namespace Telephony
+{
+    public class Smartphone : ICall, IBrowse
+    {
+
+      
+   
+        public void Browse(string browser)
+        {
+
+            bool validation = browser.Any(char.IsDigit);
+            
+            
+            if (!validation)
+            {
+                Console.WriteLine("Browsing: {0}!", browser);
+            }
+            else
+            {
+                Console.WriteLine("Invalid URL!");
+            }
+
+
+        }
+
+        public void Call(string phone)
+        {
+
+            bool validation = phone.Any(char.IsLetter);
+
+           
+
+            if (!validation)
+            {
+
+                Console.WriteLine("Calling... {0}", phone);
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+
+        }
+    }
+}
