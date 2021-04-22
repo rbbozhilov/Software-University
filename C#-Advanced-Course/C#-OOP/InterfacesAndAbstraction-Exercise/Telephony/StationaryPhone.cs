6a242b304e2ef7ca210f93185d8a094315eba71d
Rumen <rbbojilov@gmail.com>
2021-04-22 10:23:06 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/Telephony/StationaryPhone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telephony.Contracts;
+using System.Linq;
+
+namespace Telephony
+{
+   public class StationaryPhone : ICall
+    {
+
+      
+
+   
+        public void Call(string phone)
+        {
+            bool validation = phone.Any(char.IsLetter);
+
+            if (!validation)
+            {
+
+                Console.WriteLine("Dialing... {0}", phone);
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            
+        }
+    }
+}
