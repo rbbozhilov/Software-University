1152a72b41e5dc47e8c8e9b9573cb78012115f26
Rumen <rbbojilov@gmail.com>
2021-04-23 14:51:39 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/BorderControl/Robot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BorderControl
+{
+   public  class Robot : Checker
+    {
+        private string model;
+        private string id;
+
+        public Robot(string model,string id)
+        {
+            this.Model = model;
+            this.Id = id;
+        }
+
+
+        public string Model { get; }
+        
+    }
+}
