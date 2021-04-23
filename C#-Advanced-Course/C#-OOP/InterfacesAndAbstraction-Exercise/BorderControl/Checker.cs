6c7557839792c25d99fc164e7f949c09610e81c0
Rumen <rbbojilov@gmail.com>
2021-04-23 14:51:14 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/BorderControl/Checker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+   public abstract class Checker
+    {
+
+
+
+        public string Id { get; set; }
+
+
+        public string Validation(string id,string currentId)
+        {
+            if (id.Contains(currentId))
+            {
+
+                int lastIndex = id.LastIndexOf(currentId);
+                string currentString = id.Substring(lastIndex, currentId.Length);
+                lastIndex += currentId.Length;
+
+                if (lastIndex == id.Length && currentString == currentId)
+                {
+                    return id;
+                }
+
+            }
+
+
+            return "false";
+        }
+
+
+    }
+}
