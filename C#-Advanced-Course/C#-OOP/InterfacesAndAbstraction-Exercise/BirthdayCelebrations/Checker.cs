ab4cd9f376ac80717a22420952d0c2776e9467eb
Rumen <rbbojilov@gmail.com>
2021-04-25 21:51:01 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/BirthdayCelebrations/Checker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+   public abstract class Checker
+    {
+
+
+
+        public string Birthdate { get; set; }
+
+
+        public string Validation(string birthdate,string currentBirthdate)
+        {
+
+            string[] tokensBirth = birthdate.Split("/");
+            
+
+            if(tokensBirth[2]== currentBirthdate)
+            {
+                return birthdate;
+            }
+
+            else
+            {
+                return "false";
+            }
+
+        }
+
+
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/BirthdayCelebrations/Citizener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizener : Checker 
+    {
+
+        private string name;
+        private string age;
+        private string id;
+        private string birthdate;
+
+        public Citizener(string name,string age,string id,string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; }
+        public string Age { get; }
+
+       
+
+        public string Id { get; set; }
+
+
+    }
+}
