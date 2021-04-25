1307b4d46c8dc40d81cef43b0de705d7a79d3000
Rumen <rbbojilov@gmail.com>
2021-04-25 21:51:12 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/BirthdayCelebrations/Pet.cs
@@ -0,0 +1,25 @@
+﻿using BorderControl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    public class Pet : Checker
+    {
+        private string name;
+        private string birthdate;
+
+        public Pet(string name,string birthdate)
+        {
+
+            this.Name = name;
+            this.Birthdate = birthdate;
+
+        }
+
+        public string Name { get; set; }
+      
+
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/BirthdayCelebrations/Program.cs
@@ -0,0 +1,63 @@
+﻿using BorderControl;
+using System;
+using System.Collections.Generic;
+
+namespace BirthdayCelebrations
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string input = string.Empty;
+            List<Checker> list = new List<Checker>();
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] tokens = input.Split();
+                string currentCommand = tokens[0];
+
+                if (currentCommand == "Citizen")
+                {
+                    Checker current = new Citizener(tokens[1], tokens[2], tokens[3], tokens[4]);
+                    list.Add(current);
+                }
+                else if (currentCommand == "Robot")
+                {
+                    Checker current = new Robot(tokens[1], tokens[2]);
+                    list.Add(current);
+                }
+                else if (currentCommand == "Pet")
+                {
+                    Checker current = new Pet(tokens[1], tokens[2]);
+                    list.Add(current);
+                }
+
+            }
+
+            string currentInput = Console.ReadLine();
+
+            List<string> valide = new List<string>();
+
+            foreach (var items in list)
+            {
+                if(items.Birthdate==null)
+                {
+                    continue;
+                }
+
+                if (items.Validation(items.Birthdate, currentInput) != "false")
+                {
+                    valide.Add(items.Birthdate);
+                }
+
+            }
+
+            foreach (var items in valide)
+            {
+                Console.WriteLine(items);
+            }
+
+        }
+    }
+}
