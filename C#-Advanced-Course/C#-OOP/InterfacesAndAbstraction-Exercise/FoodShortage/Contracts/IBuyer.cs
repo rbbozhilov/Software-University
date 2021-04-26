56afbfba7979f9327207c98bf106fb2ed174beae
Rumen <rbbojilov@gmail.com>
2021-04-26 20:16:01 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/InterfacesAndAbstraction-Exercise/FoodShortage/Contracts/IBuyer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodShortage.Contracts
+{
+    public interface IBuyer
+    {
+
+        public string Name { get; }
+        public int Food { get; }
+
+        void BuyFood();
+
+    }
+}
