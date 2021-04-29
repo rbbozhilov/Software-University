bc5beb7b39c99e12f2fb6ca8f825eef28248b296
Rumen <rbbojilov@gmail.com>
2021-04-29 23:24:40 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Polymorphism-Lab/Shapes/Program.cs
@@ -0,0 +1,37 @@
+﻿using Shapes.Models;
+using System;
+
+namespace Shapes
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "c")
+                {
+                    Shape circle = new Circle(5);
+                    Console.WriteLine(circle.CalculateArea());
+                    Console.WriteLine(circle.CalculatePerimeter());
+                    Console.WriteLine(circle.Draw());
+                }
+
+                else if (input == "r")
+                {
+                    Shape rec = new Rectangle(4, 60);
+                    Console.WriteLine(rec.CalculateArea());
+                    Console.WriteLine(rec.CalculatePerimeter());
+                    Console.WriteLine(rec.Draw());
+                }
+
+
+            }
+
+
+        }
+    }
+}
