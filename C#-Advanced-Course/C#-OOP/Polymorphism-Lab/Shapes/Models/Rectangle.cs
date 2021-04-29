cb21f2e8e07869d537ba416677fb29196d9764ab
Rumen <rbbojilov@gmail.com>
2021-04-29 23:24:27 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Polymorphism-Lab/Shapes/Models/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Models
+{
+    public class Rectangle : Shape
+    {
+
+        private double height;
+        private double width;
+
+        public Rectangle(double height,double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+
+        public override double CalculateArea()
+        {
+            return this.height * this.width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (2 * this.height) + (2 * this.width);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + " " + this.GetType().Name;
+        }
+
+    }
+}
