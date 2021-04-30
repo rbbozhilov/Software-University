95862c221ec09f244c3639248cd8210041750bb0
Rumen <rbbojilov@gmail.com>
2021-04-30 22:53:39 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Polymorphism-Exercise/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Models
+{
+    public class Car : Vehicle
+    {
+
+
+        private const double adition = 0.9;
+
+        public Car(double fuelUantity, double fuelConsumption) : base(fuelUantity, fuelConsumption)
+        {
+        }
+
+        protected override double Adition => adition;
+
+
+
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/Polymorphism-Exercise/Models/Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Models
+{
+    public class Truck : Vehicle
+    {
+
+        private const double adition = 1.6;
+        private const double refueled = 0.95;
+        public Truck(double fuelUantity, double fuelConsumption) : base(fuelUantity, fuelConsumption)
+        {
+        }
+
+        protected override double Adition => adition;
+
+        public override void Refuel(double liters)
+        {
+            base.Refuel(liters * refueled);
+        }
+
+    }
+}
