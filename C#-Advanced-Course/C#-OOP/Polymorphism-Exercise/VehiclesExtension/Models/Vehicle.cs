ce8ac446f78e7e8235ffcf13b76c05ee00b3e4cb
Rumen <rbbojilov@gmail.com>
2021-05-01 12:00:14 +0300

    Add

---
+++ C#-Advanced-Course/C#-OOP/Polymorphism-Exercise/VehiclesExtension/Models/Vehicle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehiclesExtension.Models
+{
+    public abstract class Vehicle
+    {
+
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private double tankCapacity;
+        
+
+        public Vehicle(double fuelUantity, double fuelConsumption , double tankCapacity)
+        {
+            this.fuelQuantity = fuelUantity;
+            this.fuelConsumption = fuelConsumption;
+            this.TankCapacity = tankCapacity;
+        }
+
+        private double FuelQuantity { get { return this.fuelQuantity; } 
+            set
+            {
+
+                if (value > TankCapacity)
+                {
+                    fuelQuantity = 0;
+                }
+                else
+                {
+                    this.fuelQuantity = value;
+                }
+            } 
+        }
+        private double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }
+        private double TankCapacity { get; set; }
+      
+        protected abstract double Adition { get; }
+
+
+
+        public virtual string Driving(double distance, bool isConditioner)
+        {
+            double fuel = 0;
+
+            if (isConditioner)
+            {
+                fuel = (FuelConsumption + Adition) * distance;
+            }
+            else
+            {
+                fuel = FuelConsumption * distance;
+            }
+
+            if (fuel <= FuelQuantity)
+            {
+                FuelQuantity -= fuel;
+                return string.Format("{0} travelled {1} km", this.GetType().Name, distance);
+            }
+
+            return $"{this.GetType().Name} needs refueling";
+
+        }
+
+        public virtual void Refuel(double liters)
+        {
+
+            if (liters <= 0)
+            {
+                Console.WriteLine("Fuel must be a positive number");
+            }
+            else
+            {
+                double refuel = FuelQuantity + liters;
+
+                if (refuel > TankCapacity)
+                {
+                    Console.WriteLine("Cannot fit {0} fuel in the tank", liters);
+                }
+
+                else
+                {
+                    fuelQuantity = refuel;
+                }
+            }
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {FuelQuantity:F2}";
+        }
+
+    }
+}
---
+++ C#-Advanced-Course/C#-OOP/Polymorphism-Exercise/VehiclesExtension/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using VehiclesExtension.Models;
+
+namespace VehiclesExtension
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] inputCar = Console.ReadLine().Split();
+            Car car = new Car(double.Parse(inputCar[1]), double.Parse(inputCar[2]),double.Parse(inputCar[3]));
+            string[] inputTruck = Console.ReadLine().Split();
+            Truck truck = new Truck(double.Parse(inputTruck[1]), double.Parse(inputTruck[2]),double.Parse(inputTruck[3]));
+            string[] inputBus = Console.ReadLine().Split();
+            Bus bus = new Bus(double.Parse(inputBus[1]), double.Parse(inputBus[2]), double.Parse(inputBus[3]));
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+
+                string[] commands = Console.ReadLine().Split();
+                string currentCommand = commands[0] + " " + commands[1];
+                double currentNum = double.Parse(commands[2]);
+
+                if (currentCommand == "Drive Car")
+                {
+                    Console.WriteLine(car.Driving(currentNum,true));
+                }
+                else if (currentCommand == "Drive Truck")
+                {
+                    Console.WriteLine(truck.Driving(currentNum,true));
+                }
+                else if(currentCommand=="Drive Bus")
+                {
+                    Console.WriteLine(bus.Driving(currentNum,true));
+                }
+                else if(currentCommand=="DriveEmpty Bus")
+                {
+                    Console.WriteLine(bus.Driving(currentNum,false));
+                }
+                else if (currentCommand == "Refuel Car")
+                {
+                    car.Refuel(currentNum);
+                }
+                else if (currentCommand == "Refuel Truck")
+                {
+                    truck.Refuel(currentNum);
+                }
+                else if(currentCommand == "Refuel Bus")
+                {
+                    bus.Refuel(currentNum);
+                }
+
+            }
+
+            Console.WriteLine(car.ToString());
+            Console.WriteLine(truck.ToString());
+            Console.WriteLine(bus.ToString());
+
+        }
+    }
+}
+
