228cd59412e2ad377aae303b354f67a81303d867
Rumen Bozhilov <72761963+rbbozhilov@users.noreply.github.com>
2021-05-26 21:10:55 +0300

    Add

---
+++ JavaScript-Advanced/ObjectsAndComposition-Lab/ObjectFactory/ObjectFactory.js
@@ -0,0 +1,55 @@
+function factory(library,orders) {
+
+    let result = [];
+
+    for(let order of orders) {
+
+
+        let currentOrder = Object.assign({},order.template);
+
+        for(let part of order.parts) {
+            currentOrder[part] = library[part];
+        }
+
+        result.push(currentOrder);
+
+    }
+
+
+    return result;
+}
+
+
+
+const library = {
+    print: function () {
+      console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+      console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+      console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+  };
+  const orders = [
+    {
+      template: { name: 'ACME Printer'},
+      parts: ['print']      
+    },
+    {
+      template: { name: 'Initech Scanner'},
+      parts: ['scan']      
+    },
+    {
+      template: { name: 'ComTron Copier'},
+      parts: ['scan', 'print']      
+    },
+    {
+      template: { name: 'BoomBox Stereo'},
+      parts: ['play']      
+    },
+  ];
+  const products = factory(library, orders);
+  console.log(products);
+  
