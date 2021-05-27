8ea2341a69715ca9845d6bda609af5c72536dbb5
Rumen Bozhilov <72761963+rbbozhilov@users.noreply.github.com>
2021-05-27 23:48:19 +0300

    Add

---
+++ JavaScript-Advanced/DomIntroduction-Lab/02.Collect List Items/CollectListItems.js
@@ -0,0 +1,8 @@
+function extractText() {
+    const elements = document.getElementsByTagName('li');
+    for(let element of elements) {
+        document.getElementById('result').textContent +=element.textContent + '\n';
+    }
+   
+}
+
