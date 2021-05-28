d938850557eb4c2183d372ed3d26806522d654bc
Rumen Bozhilov <72761963+rbbozhilov@users.noreply.github.com>
2021-05-28 12:47:11 +0300

    Add

--- JavaScript-Advanced/DomIntroduction-Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JavaScript-Advanced/DomIntroduction-Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,16 @@
 function extract(content) {
+    let text = document.getElementById(content).textContent;
 
+    const regex = /\((.+?)\)/gm;
+    let match = regex.exec(text);
+    let finalResult = [];
+
+    while(match != null) {
+
+        finalResult.push(match[1]);
+        match = regex.exec(text);
+
+    }
+
+    return finalResult.join('; ');
 }
