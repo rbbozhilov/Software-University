34ae3d83a9dda3e2aea06c377f8d539b5c6c3413
Rumen Bozhilov <72761963+rbbozhilov@users.noreply.github.com>
2021-06-07 22:10:59 +0300

    Update ColorizeTable.js

--- JavaScript-Advanced/DomIntroduction-Lab/05.Colorize Table/ColorizeTable.js
+++ JavaScript-Advanced/DomIntroduction-Lab/05.Colorize Table/ColorizeTable.js
@@ -1,7 +1,5 @@
 function colorize() {
     const elementsofRows = document.querySelectorAll('table tr');
-
-   
     for(let i=1;i<elementsofRows.length;i +=2) {
         elementsofRows[i].style.backgroundColor = 'teal';
     }
