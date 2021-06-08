d85548d45da7bba3118609dd7fac4f8f05a436b2
Rumen Bozhilov <72761963+rbbozhilov@users.noreply.github.com>
2021-06-08 21:42:32 +0300

    Update ObjectFactory.js

--- JavaScript-Advanced/ObjectsAndComposition-Lab/ObjectFactory/ObjectFactory.js
+++ JavaScript-Advanced/ObjectsAndComposition-Lab/ObjectFactory/ObjectFactory.js
@@ -3,7 +3,6 @@ function factory(library,orders) {
     let result = [];
 
     for(let order of orders) {
-
         let currentOrder = Object.assign({},order.template);
 
         for(let part of order.parts) {
