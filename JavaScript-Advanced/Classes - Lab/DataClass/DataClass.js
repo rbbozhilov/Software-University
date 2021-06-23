7ebaad32371d4c59fda2572e493098387a9b13d3
Rumen Bozhilov <72761963+rbbozhilov@users.noreply.github.com>
2021-06-23 21:49:42 +0300

    Add

---
+++ JavaScript-Advanced/Classes - Lab/DataClass/DataClass.js
@@ -0,0 +1,20 @@
+class Request {
+
+
+    response = undefined;
+    fulfilled = false;
+
+    constructor(method,uri,version,message) {
+        this.method = method;
+        this.message = message;
+        this.uri  = uri;
+        this.version = version;
+       
+    }
+
+
+}
+
+
+let myData = new Request('GET', 'http://google.com', 'HTTP/1.1', '')
+console.log(myData);
