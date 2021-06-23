f1f74e29d06c00da6214520abfc2d70f682044f9
Rumen Bozhilov <72761963+rbbozhilov@users.noreply.github.com>
2021-06-23 20:48:29 +0300

    Add some classes exercise for JS

---
+++ JavaScript-Advanced/Classes - Lab/GetPerson/GetPersons.js
@@ -0,0 +1,27 @@
+
+function arrInstance() {
+
+
+class Person {
+
+    constructor(firstName,lastName,age,email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
+
+let arrOfIstances = [];
+arrOfIstances.push(new Person('Anna','Simpson',22,'anna@yahoo.com'));
+arrOfIstances.push(new Person('SoftUni'));
+arrOfIstances.push(new Person('Stephan','Johnson',25));
+arrOfIstances.push(new Person('Gabriel','Peterson',24,'g.p@gmail.com'));
+
+
+return arrOfIstances;
+}
