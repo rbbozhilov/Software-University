4b0a6c553ae14a64941be5d86135fc141338d6fe
rbbozhilov <rbbojilov@gmail.com>
2021-07-21 22:38:08 +0300

    Add some exercise

---
+++ JavaScript-Applications/AsynchronousProgramming/Gighub-Commits/app.js
@@ -0,0 +1,40 @@
+function loadCommits() {
+    
+    let ulElement = document.getElementById('commits');
+    let username = document.getElementById('username');
+    let repo = document.getElementById('repo');
+    let url = `https://api.github.com/repos/${username.value}/${repo.value}/commits`;
+
+    fetch(url)
+      .then((response) => {
+
+        if(!response.ok) {
+            let liElement = document.createElement('li');
+            liElement.textContent = `Error: ${response.status} (Not Found)`
+            ulElement.appendChild(liElement);
+            throw new Error();
+        }
+
+          return response.json();
+      })
+      .then((dataFromResponse) => {
+
+        dataFromResponse.forEach(currentData => {
+
+            let liElement = document.createElement('li');
+            liElement.textContent = `${currentData.commit.author.name}: ${currentData.commit.message}`;
+            ulElement.appendChild(liElement);
+        });
+
+
+      })
+      .catch((error) => {
+
+
+
+      }) 
+
+
+
+
+}
---
+++ JavaScript-Applications/AsynchronousProgramming/Github-Repos/scripts/app.js
@@ -0,0 +1,34 @@
+async function loadRepos() {
+	let currentUsername = document.getElementById('username').value;
+	let baseURL = `https://api.github.com/users/${currentUsername}/repos`;
+
+	try {
+
+	
+	let responseURL = await fetch(baseURL);
+
+	if(!responseURL.ok) {
+		throw new Error(`${responseURL.status} is not ok`);
+	}
+
+	let dataURL = await responseURL.json();
+
+	let ulElement = document.getElementById('repos');
+	ulElement.innerHTML = '';
+
+	dataURL.forEach(element => {
+		
+		let liElement = document.createElement('li');
+		liElement.textContent = element.full_name;
+		ulElement.appendChild(liElement);
+
+	});
+
+	} catch(error) {
+
+		console.log(error);
+
+
+	}
+
+}
